b680d8c76f3dad0b1cd4fe22de219fa57acc6c64
amarshal <amarshal@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2009-03-03 23:07:54 +0000

    Reverting to (an equivalent to) the old BML gaze speed defaults, despite the insanely high head speed.
    It looks great in SASO, but also looks good in test-gaze-bml-aversion.  Honestly, there seems to be more "snap" with the same smoothing with these very large numbers than there is with the more reasonable numbers.
    
    
    
    Also, corrected a log string in bml_face.cpp

--- core/smartbody/smartbody-lib/src/sbm/me_ct_gaze.cpp
+++ core/smartbody/smartbody-lib/src/sbm/me_ct_gaze.cpp
@@ -49,11 +49,11 @@ const float MeCtGaze::DEFAULT_LIMIT_ROLL[]       = { 10.0,     5.0,    20.0,    
 //   Brent's Edits / Gaze Paper Update (Oct2008):
 //     Speed     (head, eyes, default eyes arg): 180, 1000, 10000
 //     Smoothing (back, neck, eyes): 0.3, 0.1, 0.0
-//   Andrew's Updates (Feb2009):
-//     Speed     (head, eyes, default eyes arg): 360, 1000, 10000
+//   Andrew's Updates, equiv to old BML defaults (Feb2009):
+//     Speed     (head, eyes, default eyes arg): 360, 10000, 10000
 //     Smoothing (back, neck, eyes): 0.8, 0.8, 0.1
-const float MeCtGaze::DEFAULT_SPEED_HEAD         = 360;
-const float MeCtGaze::DEFAULT_SPEED_EYES         = 1000.0;
+const float MeCtGaze::DEFAULT_SPEED_HEAD         = 2500;
+const float MeCtGaze::DEFAULT_SPEED_EYES         = 10000.0;
 
 const float MeCtGaze::DEFAULT_SMOOTHING_LUMBAR   = 0.8f; 
 const float MeCtGaze::DEFAULT_SMOOTHING_CERVICAL = 0.8f;
