85a77f611a0a3477dafa74015114389e1fac45ec
thiebaux <thiebaux@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2009-03-11 20:35:21 +0000

    Added BML params for MeCtQuickDraw controller  smooth <float>  gundraw-duration <float>  holster-duration <float>  aim-offset <pitch heading roll>

--- core/smartbody/smartbody-lib/src/sbm/bml_quickdraw.cpp
+++ core/smartbody/smartbody-lib/src/sbm/bml_quickdraw.cpp
@@ -37,9 +37,13 @@
 
 
 ////// XML ATTRIBUTES
-const XMLCh ATTR_ROLL[]         = L"roll";
 const XMLCh ATTR_ANIM[]         = L"anim";
+const XMLCh ATTR_SMOOTH[]       = L"smooth";
 const XMLCh ATTR_TRACK_DUR[]    = L"track-duration";
+const XMLCh ATTR_GUNDRAW_DUR[]   = L"gundraw-duration";
+const XMLCh ATTR_HOLSTER_DUR[]   = L"holster-duration";
+const XMLCh ATTR_AIM_OFFSET[]    = L"aim-offset";
+
 
 
 char* DEFAULT_QUICKDRAW_ANIM	= "AdultM_FastDraw001";
@@ -69,10 +73,6 @@ BehaviorRequestPtr BML::parse_bml_quickdraw( DOMElement* elem,
 		return BehaviorRequestPtr();  // a.k.a., NULL
 	}
 
-	const XMLCh* attrRoll = elem->getAttribute( ATTR_ROLL );
-	if( !attrTarget || !XMLString::stringLen( attrTarget ) ) {
-	}
-
 	string anim_name( DEFAULT_QUICKDRAW_ANIM );
 	const XMLCh* attrAnim = elem->getAttribute( ATTR_ANIM );
 	if( attrTarget && XMLString::stringLen( attrAnim )>0 ) {
@@ -99,10 +99,80 @@ BehaviorRequestPtr BML::parse_bml_quickdraw( DOMElement* elem,
 		XMLString::release( &temp_ascii );
 	}
 
+	bool set_gundraw_dur_param = false;
+	float gundraw_dur = 0.0;
+	const XMLCh* attrGundrawDur = elem->getAttribute( ATTR_GUNDRAW_DUR );
+	if( attrGundrawDur && XMLString::stringLen( attrGundrawDur )>0 ) {
+		char* temp_ascii = XMLString::transcode( attrGundrawDur );
+		string parse_buffer( temp_ascii );
+		istringstream parser( parse_buffer );
+		if( !( parser >> gundraw_dur ) ) {
+			wcerr << "WARNING: BML::parse_bml_quickdraw(): Attribute "<<ATTR_GUNDRAW_DUR<<"=\""<<attrGundrawDur<<"\" is not a valid number." << endl;
+		}
+		else	{
+			set_gundraw_dur_param = true;
+		}
+		XMLString::release( &temp_ascii );
+	}
+
+	bool set_holster_dur_param = false;
+	float holster_dur = 0.0;
+	const XMLCh* attrHolsterDur = elem->getAttribute( ATTR_HOLSTER_DUR );
+	if( attrHolsterDur && XMLString::stringLen( attrHolsterDur )>0 ) {
+		char* temp_ascii = XMLString::transcode( attrHolsterDur );
+		string parse_buffer( temp_ascii );
+		istringstream parser( parse_buffer );
+		if( !( parser >> gundraw_dur ) ) {
+			wcerr << "WARNING: BML::parse_bml_quickdraw(): Attribute "<<ATTR_HOLSTER_DUR<<"=\""<<attrHolsterDur<<"\" is not a valid number." << endl;
+		}
+		else	{
+			set_holster_dur_param = true;
+		}
+		XMLString::release( &temp_ascii );
+	}
+
+	bool set_aim_offset_param = false;
+	float aim_offset_p = 0.0;
+	float aim_offset_h = 0.0;
+	float aim_offset_r = 0.0;
+	const XMLCh* attrAimOffset = elem->getAttribute( ATTR_AIM_OFFSET );
+	if( attrAimOffset && XMLString::stringLen( attrAimOffset )>0 ) {
+		char* temp_ascii = XMLString::transcode( attrAimOffset );
+		string parse_buffer( temp_ascii );
+		istringstream parser( parse_buffer );
+		if( !( parser >> aim_offset_p >> aim_offset_h >> aim_offset_r ) ) {
+			wcerr << "WARNING: BML::parse_bml_quickdraw(): Attribute "<<ATTR_AIM_OFFSET<<"=\""<<attrAimOffset<<"\" is not valid." << endl;
+		}
+		else	{
+			set_aim_offset_param = true;
+		}
+		XMLString::release( &temp_ascii );
+	}
+
+	bool set_smooth_param = false;
+	float smooth_factor = 0.0;
+	const XMLCh* attrSmooth = elem->getAttribute( ATTR_SMOOTH );
+	if( attrSmooth && XMLString::stringLen( attrSmooth )>0 ) {
+		char* temp_ascii = XMLString::transcode( attrSmooth );
+		string parse_buffer( temp_ascii );
+		istringstream parser( parse_buffer );
+		if( !( parser >> smooth_factor ) ) {
+			wcerr << "WARNING: BML::parse_bml_quickdraw(): Attribute "<<ATTR_SMOOTH<<"=\""<<attrSmooth<<"\" is not a valid number." << endl;
+		}
+		else	{
+			set_smooth_param = true;
+		}
+		XMLString::release( &temp_ascii );
+	}
+
 	MeCtQuickDraw* qdraw_ct = new MeCtQuickDraw();
 	qdraw_ct->init( anim );
 	qdraw_ct->set_target_joint( 0, 0, 0, const_cast<SkJoint*>(joint) );
 	qdraw_ct->set_track_duration( track_duration );
+	if( set_gundraw_dur_param ) qdraw_ct->set_gundraw_duration( gundraw_dur );
+	if( set_holster_dur_param ) qdraw_ct->set_holster_duration( holster_dur );
+	if( set_aim_offset_param ) qdraw_ct->set_aim_offset( aim_offset_p, aim_offset_h, aim_offset_r );
+	if( set_smooth_param ) qdraw_ct->set_smooth( smooth_factor );
 
 	return BehaviorRequestPtr( new MeControllerRequest( unique_id, qdraw_ct, request->actor->motion_sched_p, true, tms ) );
 }
