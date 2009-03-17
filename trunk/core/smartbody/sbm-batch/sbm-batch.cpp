1d6fde25d231e96a46c5ff27410853871928da61
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2009-03-17 23:00:12 +0000

    sbm-batch - adding console tool for running smartbody related batch functions

---
+++ trunk/core/smartbody/sbm-batch/sbm-batch.cpp
@@ -0,0 +1,32 @@
+/*
+ *  sbm-batch.cpp - part of SBM: SmartBody Module
+ *  Copyright (C) 2008  University of Southern California
+ *
+ *  SBM is free software: you can redistribute it and/or
+ *  modify it under the terms of the Lesser GNU General Public License
+ *  as published by the Free Software Foundation, version 3 of the
+ *  license.
+ *
+ *  SBM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Lesser GNU General Public License for more details.
+ *
+ *  You should have received a copy of the Lesser GNU General Public
+ *  License along with SBM.  If not, see:
+ *      http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ *  CONTRIBUTORS:
+ *      Marcus Thiebaux, USC
+ */
+
+
+#include <stdio.h>
+
+
+int main( int argc, char ** argv )
+{
+   printf( "Hello World\n" );
+
+   return 0;
+}
