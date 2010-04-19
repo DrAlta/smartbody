e8c963e5d79d98b29832ada0efa93624b887b614
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-04-19 20:11:07 +0000

    Additional resource information (keeps track of commands).

--- core/smartbody/smartbody-lib/src/sbm/ResourceManager.cpp
+++ core/smartbody/smartbody-lib/src/sbm/ResourceManager.cpp
@@ -5,18 +5,54 @@ ResourceManager* ResourceManager::manager = NULL;
 
 ResourceManager::ResourceManager()
 {
+	last_resource = NULL;
+	is_seq_cmd = false;
+	last_seq_cmd_name = "";
 }
 
 ResourceManager::~ResourceManager()
 {
 	for (unsigned int r = 0; r < resources.size(); r++)
+	{
 		delete resources[r];
+	}
 }
 
 void ResourceManager::addResource(Resource* r)
 {
-	resources.push_back(r);
+	// set up a limit to the number of resources that can be stored
+	// since too many resources stored may cause excessive memory 
+	// problems for a long-running application
+	//if (maxNumResources > 1000) // or something like that
+	//{
+	//}
+
+	CmdResource* cmd = dynamic_cast<CmdResource*>(r);
+	if (cmd)
+	{
+		if (cur_parent.size() > 0)
+		{
+			cur_parent.top()->addChild(r);
+			last_resource = cmd;
+		}
+		else
+		{
+			resources.push_back(r);
+			last_resource = cmd;
+		}
+	}
+	else
+	{
+		resources.push_back(r);
+	}
 }
+
+void ResourceManager::removeParent()
+{
+	if (cur_parent.size() > 0)
+		cur_parent.pop();
+}
+
 
 int ResourceManager::getNumResources()
 {
@@ -39,4 +75,77 @@ ResourceManager* ResourceManager::getResourceManager()
 	}
 
 	return manager;
+}
+
+void ResourceManager::addParent(Resource* parent)
+{
+	cur_parent.push(parent);
+}
+
+Resource* ResourceManager::getParent()
+{
+	return cur_parent.top();
+}
+
+
+Resource* ResourceManager::getLastCmdResource()
+{
+	return last_resource;
+}
+
+CmdResource* ResourceManager::getCmdResource(std::string id)
+{
+
+	for (unsigned int r = 0; r < resources.size(); r++)
+	{
+		CmdResource* cmd = dynamic_cast<CmdResource*>(resources[r]);
+		if (cmd)
+		{
+			CmdResource* c = getCmdResourceRecurse(id, cmd);
+			if (c)
+				return c;
+		}
+		
+	}
+	// ...
+	// ...
+	return NULL;
+}
+
+CmdResource* ResourceManager::getCmdResourceRecurse(std::string id, CmdResource* r)
+{
+	if (r->getId() == id)
+		return r;
+
+
+	for (int c = 0; c < r->getNumChildren(); c++)
+	{	
+		CmdResource* childCmd = dynamic_cast<CmdResource*>(r->getChild(c));
+		CmdResource* cmd = getCmdResourceRecurse(id, childCmd);
+		if (cmd)
+			return cmd;
+	}
+
+	return NULL;
+}
+
+
+void ResourceManager::setIsSeqCmd(bool val)
+{
+	is_seq_cmd = val;
+}
+
+bool ResourceManager::isSeqCmd()
+{
+	return is_seq_cmd;
+}
+
+std::string ResourceManager::getSeqCmdName()
+{
+	return last_seq_cmd_name;
+}
+
+void ResourceManager::setSeqCmdName(std::string seqcmd)
+{
+	last_seq_cmd_name = seqcmd;
 }
--- core/smartbody/smartbody-lib/src/sbm/ResourceManager.h
+++ core/smartbody/smartbody-lib/src/sbm/ResourceManager.h
@@ -3,6 +3,7 @@
 
 #include "Resource.h"
 #include <vector>
+#include <stack>
 
 class ResourceManager
 {
@@ -14,10 +15,28 @@ class ResourceManager
 		int getNumResources();
 		Resource* getResource(unsigned int index);
 
+		void addParent(Resource* parent);
+		void removeParent();
+		Resource* getParent();
+		Resource* getLastCmdResource();
+		CmdResource* getCmdResource(std::string id);
+
+		void setIsSeqCmd(bool val);
+		bool isSeqCmd();
+		std::string getSeqCmdName();
+		void setSeqCmdName(std::string seqcmd);
+
 		static ResourceManager* getResourceManager();
+		
 	
 	private:
+		CmdResource* getCmdResourceRecurse(std::string id, CmdResource* r);
+		
 		std::vector<Resource*> resources;
 		static ResourceManager* manager;
+		std::stack<Resource*> cur_parent;
+		CmdResource* last_resource;
+		std::string last_seq_cmd_name;
+		bool is_seq_cmd;
 };
 #endif
