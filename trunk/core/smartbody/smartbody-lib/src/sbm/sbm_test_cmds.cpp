4917e9b53db517b223486268e5ea1adebd8c7459
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-05-26 21:42:23 +0000

    sbm - changed process_id to a std::string.

--- trunk/core/smartbody/smartbody-lib/src/sbm/sbm_test_cmds.cpp
+++ trunk/core/smartbody/smartbody-lib/src/sbm/sbm_test_cmds.cpp
@@ -205,7 +205,7 @@ void build_vrX( ostringstream& buffer, const string& cmd, const string& char_id,
 	if( for_seq )
 		buffer << "send " << cmd << " ";
 	buffer << char_id << " "<< recip_id << " sbm";
-	if( mcu.process_id )  // Insert process_id if present.
+	if( mcu.process_id != "" )  // Insert process_id if present.
 		buffer << '_' << mcu.process_id; 
 	buffer << "_test_bml_" << (++test_bml_id) << endl << content;
 }
