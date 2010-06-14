e7d025ef1189bb0af63f4949ee2904b78750807c
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-06-14 23:04:21 +0000

    exporters - merging changes from my branch.  Things brings the trunk up-do-date with the exporters.

---
+++ trunk/core/smartbody/Maya mel scripts/run_skmExporter_sample.bat
@@ -0,0 +1,5 @@
+
+@rem usage:  call skmExporter.bat "<source maya file>" "<dest dir>/"
+
+call skmExporter.bat "C:/art/SBM_MayaProject/scenes/sbm_Proxy2.ma" "./New Folder/"
+call skmExporter.bat "C:/art/SBM_MayaProject/scenes/sbm_Proxy2.ma" ""
