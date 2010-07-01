9cf7f5ce9fb6ae9e22d4a8013736941ad5e968d7
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-07-01 20:55:31 +0000

    modified configuration file

--- trunk/test_suite/testconfig.sh
+++ trunk/test_suite/testconfig.sh
@@ -1,5 +1,6 @@
-BASEDIR=`pwd`
+CYGBASEDIR=`pwd`
+BASEDIR=`cygpath -m $CYGBASEDIR`
 SBMBIN=$BASEDIR/../core/smartbody/sbm/bin
-OUTPUTDIR=$BASEDIR/output/
-INPUTDIR=$BASEDIR/input/
+OUTPUTDIR=$BASEDIR/output
+INPUTDIR=$BASEDIR/input
 SBMEXE=sbm-fltkd.exe
