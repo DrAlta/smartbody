d5728e83cc78cf5f3aebf39a21fa45507fccfade
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-07-16 20:25:06 +0000

    delete unnecessary scripts

--- trunk/test_suite/common_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd $SBMBIN
-./$SBMEXE $SBMINPUT
-cd -
--- trunk/test_suite/testconfig.sh
+++
@@ -1,5 +0,0 @@
-BASEDIR=`pwd`
-SBMBIN=$BASEDIR/../core/smartbody/sbm/bin
-OUTPUTDIR=$BASEDIR/output
-INPUTDIR=$BASEDIR/input
-SBMEXE=sbm-fltkd.exe
