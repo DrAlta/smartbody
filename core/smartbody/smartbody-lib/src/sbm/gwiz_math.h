dcfa31c4a56e760a3ed8a0173d2fa1a5116c1689
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-10-27 18:41:24 +0000

    fixed a bug in matrix transpose function

--- core/smartbody/smartbody-lib/src/sbm/gwiz_math.h
+++ core/smartbody/smartbody-lib/src/sbm/gwiz_math.h
@@ -575,9 +575,9 @@ class matrix_t {
 			tmp = M[0][1]; M[0][1] = M[1][0]; M[1][0] = tmp;
 			tmp = M[0][2]; M[0][2] = M[2][0]; M[2][0] = tmp;
 			tmp = M[0][3]; M[0][3] = M[3][0]; M[3][0] = tmp;
-			tmp = M[1][2]; M[2][1] = M[1][2]; M[1][2] = tmp;
-			tmp = M[1][3]; M[3][1] = M[1][3]; M[1][3] = tmp;
-			tmp = M[2][3]; M[3][2] = M[2][3]; M[2][3] = tmp;
+			tmp = M[1][2]; M[1][2] = M[2][1]; M[1][2] = tmp;
+			tmp = M[1][3]; M[1][3] = M[3][1]; M[3][1] = tmp;
+			tmp = M[2][3]; M[2][3] = M[3][2]; M[3][2] = tmp;
 			return( *this );
 		}
         matrix_t inverse( void ) const;
