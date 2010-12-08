7521e8e8a07b24241bb98efa185a0f8c7c1b2692
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2010-12-08 22:03:38 +0000

    sbm - removed #include "vhcl.h" from header.  vhcl.h should only be added to .cpp files.  Moved VisemeData() ctor into .cpp, until all .cpp files have adopted vhcl.

--- core/smartbody/smartbody-lib/src/sbm/sbm_speech.cpp
+++ core/smartbody/smartbody-lib/src/sbm/sbm_speech.cpp
@@ -20,11 +20,39 @@
  *      Andrew n marshall, USC
  */
 
+#include "vhcl.h"
+
 #include "sbm_speech.hpp"
 
 
 using namespace std;
 using namespace SmartBody;
+
+
+VisemeData::VisemeData( const char * id, int numKeys, const char * curveInfo )
+	: _id( id ), _weight( 1.0 ), _time( 0.0 ), _duration( 0.0 ), _numKeys( numKeys ), _curveInfo( curveInfo ), _curveMode(true)
+{
+	// NOTE: We are parsing twice - once here in order to extract the proper duration of the curve,
+	//       and later again when processing the command. These two parses should be unified here for
+	//       efficiency reasons. The parse below is a 'light' parse - although we need to tokenize the
+	//       entire curve data string, we are only interested in the last control point.
+
+	std::vector<std::string> tokens;
+	vhcl::Tokenize(curveInfo, tokens, " ");
+	if (tokens.size() > 0 && numKeys > 0)
+	{
+		// make sure the curve info is properly created
+		int index = 4 * (numKeys - 1);
+		if (tokens.size() <= (unsigned int) index)
+		{
+			LOG("Curve %s is not properly created - insufficient number of keys (%d) - expected %d.", id, tokens.size() / 4, numKeys * 4);
+		}
+		else
+		{
+			_duration = float(atof(tokens[index].c_str()));
+		}
+	}
+}
 
 
 ostream& operator << ( ostream& strm, const VisemeData& v ) {
