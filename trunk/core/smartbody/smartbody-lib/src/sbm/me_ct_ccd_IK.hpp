c63563deba2f64be787db4b08c7d8b0fc7af12d7
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-03-22 18:34:21 +0000

    Add missing files to fix build error.

---
+++ trunk/core/smartbody/smartbody-lib/src/sbm/me_ct_ccd_IK.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "me_ct_jacobian_IK.hpp"
+
+// feng : this class is basically a re-write for MeCtReachIK ( a CCD based IK solver derived from MeCtIK, with more shared code 
+// and better readibility. By using the tree structure by default ( since IK-chain is just a special case of the tree ), this provides 
+// potential extension to handling & blending multiple IK chains in the solver.
+
+class MeCtCCDIK : public MeCtIKInterface	
+{	
+protected:	
+	MeCtIKTreeScenario* ikScenario;
+public:
+	MeCtCCDIK(void);
+	~MeCtCCDIK(void);
+public:
+	virtual void update(MeCtIKTreeScenario* scenario);	
+	static gwiz::vector_t quat2SwingTwist(gwiz::quat_t& quat);
+	static gwiz::quat_t   swingTwist2Quat(gwiz::vector_t& quat);
+protected:
+	void CCDRotate(MeCtIKTreeNode* startNode, MeCtIKTreeNode* endEffector);
+};
