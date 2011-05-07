3cd05d86d4b2b703d7d5883529e180a14f41c6ac
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-05-07 22:29:30 +0000

    ogre-viewer - simplified postbuild steps.  Also, always copy lib\OgreSDK\media over to bin folder so that \lib isn't required for small dist build.

--- trunk/core/ogre-viewer/build/postbuild.bat
+++ trunk/core/ogre-viewer/build/postbuild.bat
@@ -9,53 +9,78 @@ set OGRE_HOME=..\..\..\lib\OgreSDK
 if (%1)==(DEBUG) goto DEBUG_VARS
 goto RELEASE_VARS
 
+
 :RELEASE_VARS
 set BIN_DIR=..\bin\win32
 set PLUGINS_FILE=..\src\plugins-win32.cfg
 set OGRE_BIN=%OGRE_HOME%\bin\release
-set OGRE_FILES=OgreMain.dll;OIS.dll;RenderSystem_Direct3D9.dll;RenderSystem_GL.dll
+set FINAL_BIN_DIR=..\..\..\bin\ogre-viewer\bin\win32
+set FINAL_MEDIA_DIR=..\..\..\bin\ogre-viewer\media
+
+copy /Y %PLUGINS_FILE% %BIN_DIR%\plugins.cfg
+
+copy /Y %OGRE_BIN%\OgreMain.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\OIS.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\RenderSystem_Direct3D9.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\RenderSystem_GL.dll %BIN_DIR%
+
 copy /Y ..\..\..\lib\pthreads\lib\pthreadVSE2.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr\lib\libapr-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-iconv\lib\libapriconv-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-util\lib\libaprutil-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\activemq-cpp\vs2008-build\ReleaseDLL\activemq-cpp.dll %BIN_DIR%
-set FINAL_BIN_DIR=..\..\..\bin\ogre-viewer\bin\win32
-set FINAL_MEDIA_FOLDER=..\..\..\bin\ogre-viewer\media
-goto COPY
+
+if not exist ..\media\OgreSDK\media\packs   mkdir ..\media\OgreSDK\media\packs
+if not exist ..\media\OgreSDK\media\models  mkdir ..\media\OgreSDK\media\models
+copy /Y %OGRE_HOME%\media\packs\OgreCore.zip  ..\media\OgreSDK\media\packs
+copy /Y %OGRE_HOME%\media\models\cube.mesh    ..\media\OgreSDK\media\models
+copy /Y %OGRE_HOME%\media\models\sphere.mesh  ..\media\OgreSDK\media\models
+
+copy /Y ..\media\resources.cfg  %BIN_DIR%
+
+xcopy /Y /Q /E /I /D %BIN_DIR% %FINAL_BIN_DIR%
+xcopy /Y /Q /E /I /D ..\media %FINAL_MEDIA_DIR%
+
+@goto END
+
 
 :DEBUG_VARS
 set BIN_DIR=..\bin\win32_debug
 set PLUGINS_FILE=..\src\plugins-win32_debug.cfg
 set OGRE_BIN=%OGRE_HOME%\bin\debug
-set OGRE_FILES=OgreMain_d.dll;OIS_d.dll;RenderSystem_Direct3D9_d.dll;RenderSystem_GL_d.dll
+set FINAL_BIN_DIR=..\..\..\bin\ogre-viewer\bin\win32_debug
+set FINAL_MEDIA_DIR=..\..\..\bin\ogre-viewer\media
+
+copy /Y %PLUGINS_FILE% %BIN_DIR%\plugins.cfg
+
+copy /Y %OGRE_BIN%\OgreMain_d.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\OIS_d.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\RenderSystem_Direct3D9_d.dll %BIN_DIR%
+copy /Y %OGRE_BIN%\RenderSystem_GL_d.dll %BIN_DIR%
+
 copy /Y ..\..\..\lib\pthreads\lib\pthreadVSE2.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr\lib\libapr-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-iconv\lib\libapriconv-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-util\lib\libaprutil-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\activemq-cpp\vs2008-build\DebugDLL\activemq-cppd.dll %BIN_DIR%
-set FINAL_BIN_DIR=..\..\..\bin\ogre-viewer\bin\win32_debug
-set FINAL_MEDIA_FOLDER=..\..\..\bin\ogre-viewer\media
-goto COPY
 
-:COPY
-@REM Copy files
-copy %PLUGINS_FILE% %BIN_DIR%\plugins.cfg
-for %%F in (%OGRE_FILES%) do echo %%F && COPY %OGRE_BIN%\%%F %BIN_DIR%
-@REM expand "%DXSDK_DIR%\Redist\JUN2008_d3dx9_38_x86.cab" -F:d3dx9_38.dll %BIN_DIR%
+if not exist ..\media\OgreSDK\media\packs   mkdir ..\media\OgreSDK\media\packs
+if not exist ..\media\OgreSDK\media\models  mkdir ..\media\OgreSDK\media\models
+copy /Y %OGRE_HOME%\media\packs\OgreCore.zip  ..\media\OgreSDK\media\packs
+copy /Y %OGRE_HOME%\media\models\cube.mesh    ..\media\OgreSDK\media\models
+copy /Y %OGRE_HOME%\media\models\sphere.mesh  ..\media\OgreSDK\media\models
 
-call copy_media.bat SHARE %BIN_DIR%
+copy /Y ..\media\resources.cfg  %BIN_DIR%
 
-if not exist %FINAL_BIN_DIR% mkdir %FINAL_BIN_DIR%
-copy %BIN_DIR%\*.* %FINAL_BIN_DIR%
-
-if not exist %FINAL_MEDIA_FOLDER%  mkdir %FINAL_MEDIA_FOLDER%
-copy ..\media\*.* %FINAL_MEDIA_FOLDER%
-
+xcopy /Y /Q /E /I /D %BIN_DIR% %FINAL_BIN_DIR%
+xcopy /Y /Q /E /I /D ..\media %FINAL_MEDIA_DIR%
 
 @goto END
 
+
 :ERROR
-@echo ERROR in copy_media.bat: %ERROR_MSG%
+@echo ERROR in postbuild.bat: %ERROR_MSG%
 @set ERRORLEV=1
+
 
 :END
