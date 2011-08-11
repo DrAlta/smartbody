8dadbc57a7c39dbd1d04c646cf423eedf71b92f7
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-08-11 22:40:11 +0000

    ogre-viewer - added python26.dll and boost-python.dll to post-build steps

--- core/ogre-viewer/build/postbuild.bat
+++ core/ogre-viewer/build/postbuild.bat
@@ -32,9 +32,11 @@ copy /Y ..\..\..\lib\xerces-c\bin\xerces-c_3_0.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_filesystem-vc90-mt-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_system-vc90-mt-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_regex-vc90-mt-1_44.dll %BIN_DIR%
+copy /Y ..\..\..\lib\boost\lib\boost_python-vc90-mt-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\vhcl\log4cxx\lib\log4cxx.dll %BIN_DIR%
 copy /Y ..\..\..\core\smartbody\steersuite-1.3\build\win32\Release\steerlib.dll %BIN_DIR%
 copy /Y ..\..\..\core\smartbody\steersuite-1.3\build\win32\Release\pprAI.dll %BIN_DIR%
+copy /Y ..\..\..\core\smartbody\python26\python26.dll %BIN_DIR%
 copy /Y ..\..\..\lib\pthreads\lib\pthreadVSE2.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr\lib\libapr-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-iconv\lib\libapriconv-1.dll %BIN_DIR%
@@ -83,9 +85,11 @@ copy /Y ..\..\..\lib\xerces-c\bin\xerces-c_3_0D.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_filesystem-vc90-mt-gd-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_system-vc90-mt-gd-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\boost\lib\boost_regex-vc90-mt-gd-1_44.dll %BIN_DIR%
+copy /Y ..\..\..\lib\boost\lib\boost_python-vc90-mt-gd-1_44.dll %BIN_DIR%
 copy /Y ..\..\..\lib\vhcl\log4cxx\lib\log4cxxd.dll %BIN_DIR%
 copy /Y ..\..\..\core\smartbody\steersuite-1.3\build\win32\Debug\steerlibd.dll %BIN_DIR%
 copy /Y ..\..\..\core\smartbody\steersuite-1.3\build\win32\Debug\pprAId.dll %BIN_DIR%
+copy /Y ..\..\..\core\smartbody\python26\python26.dll %BIN_DIR%
 copy /Y ..\..\..\lib\pthreads\lib\pthreadVSE2.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr\lib\libapr-1.dll %BIN_DIR%
 copy /Y ..\..\..\lib\activemq\apr\apr-iconv\lib\libapriconv-1.dll %BIN_DIR%
