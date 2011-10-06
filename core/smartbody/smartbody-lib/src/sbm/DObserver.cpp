7ef88782e53c5b24cfc07dfd8d7a5b764d157996
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-10-06 21:30:54 +0000

    Better handling of attributes on object deletion.

--- core/smartbody/smartbody-lib/src/sbm/DObserver.cpp
+++ core/smartbody/smartbody-lib/src/sbm/DObserver.cpp
@@ -1,4 +1,7 @@
 #include "DObserver.h"
+#include "DSubject.h"
+
+#include <vector>
 
 DObserver::DObserver()
 {
@@ -6,5 +9,36 @@ DObserver::DObserver()
 
 DObserver::~DObserver()
 {
+	std::vector<DSubject*> tmpSubjects;
+	for (std::set<DSubject*>::iterator iter = _subjects.begin(); 
+		 iter != _subjects.end();
+		 iter++)
+	{
+		tmpSubjects.push_back(*iter);
+	}
+	
+	for (std::vector<DSubject*>::iterator iter = tmpSubjects.begin(); 
+		 iter != tmpSubjects.end();
+		 iter++)
+	{
+		(*iter)->unregisterObserver(this);
+	}
+}
+
+void DObserver::addDependency(DSubject* subject)
+{
+	if (_subjects.find(subject) == _subjects.end())
+	{
+		_subjects.insert(subject);
+	}
+}
+
+void DObserver::removeDependency(DSubject* subject)
+{
+	std::set<DSubject*>::iterator iter = _subjects.find(subject);
+	if (iter != _subjects.end())
+	{
+		_subjects.erase(iter);
+	}
 }
 
