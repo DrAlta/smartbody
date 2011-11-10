260a8ae5b51b7129794db76e4ac2782d1da0120f
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-11-10 21:18:42 +0000

    Missing SBScript files from last checkin.

---
+++ trunk/core/smartbody/smartbody-lib/src/sbm/SBScript.cpp
@@ -0,0 +1,33 @@
+#include "SBScript.h"
+
+namespace SmartBody {
+
+SBScript::SBScript() : SBObject()
+{
+}
+
+SBScript::~SBScript()
+{
+}
+
+void SBScript::start()
+{
+}
+
+void SBScript::beforeUpdate()
+{
+}
+
+void SBScript::update(double time)
+{
+}
+
+void SBScript::afterUpdate()
+{
+}
+
+void SBScript::stop()
+{
+}
+
+}
