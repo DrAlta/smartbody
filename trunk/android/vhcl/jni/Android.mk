18cea343b42071791b7ecd0ff2516aaf9c738761
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-11-10 22:40:53 +0000

    Add revised vhcl libs for android build.

--- trunk/android/vhcl/jni/Android.mk
+++ trunk/android/vhcl/jni/Android.mk
@@ -2,13 +2,17 @@
 #
 
 VHCL_LOCAL_PATH := $(call my-dir)
+
+include $(VHCL_LOCAL_PATH)/../../openal/android/jni/Android.mk
+include $(VHCL_LOCAL_PATH)/../../libsndfile/jni/Android.mk
+
 LOCAL_PATH = $(VHCL_LOCAL_PATH)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vhcl
 TARGET_PLATFORM := android-9
 MY_VHCL_DIR := ../../../lib/vhcl/
 LOCAL_CFLAGS    := -DBUILD_ANDROID
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(MY_VHCL_DIR)/include $(LOCAL_PATH)/$(MY_VHCL_DIR)/log4cxx/src/main/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(MY_VHCL_DIR)/include $(LOCAL_PATH)/$(MY_VHCL_DIR)/log4cxx/src/main/include $(LOCAL_PATH)/../../openal/include $(LOCAL_PATH)/../../libsndfile/include
 LOCAL_SRC_FILES := $(MY_VHCL_DIR)/src/Pow2Assert.cpp \
 				   $(MY_VHCL_DIR)/src/vhcl_audio.cpp \
 				   $(MY_VHCL_DIR)/src/vhcl_log.cpp \
@@ -16,6 +20,6 @@ LOCAL_SRC_FILES := $(MY_VHCL_DIR)/src/Pow2Assert.cpp \
 				   $(MY_VHCL_DIR)/src/vhcl_timer.cpp \
 				   $(MY_VHCL_DIR)/src/vhcl_memory.cpp \
 				   $(MY_VHCL_DIR)/src/vhcl_crash.cpp 	
-
+LOCAL_STATIC_LIBRARIES := openal sndfile openalut
 				   
 include $(BUILD_STATIC_LIBRARY)
