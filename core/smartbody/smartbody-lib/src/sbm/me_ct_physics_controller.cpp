1ad89c875b732a549c02f7bb1ec2d0eb916b0e2a
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-11-21 23:02:25 +0000

    Add missing files for physics controller.

---
+++ core/smartbody/smartbody-lib/src/sbm/me_ct_physics_controller.cpp
@@ -0,0 +1,71 @@
+#include "me_ct_physics_controller.h"
+#include <sr/sr_euler.h>
+#include <sbm/mcontrol_util.h>
+
+std::string MeCtPhysicsController::CONTROLLER_TYPE = "PhysicsController";
+
+MeCtPhysicsController::MeCtPhysicsController(SbmCharacter* character) : SmartBody::SBController()
+{
+	_character = character;
+	_valid = true;
+	_prevTime = 0.0;
+	_duration = 0.0;
+}
+
+MeCtPhysicsController::~MeCtPhysicsController()
+{
+	
+}
+
+
+bool MeCtPhysicsController::controller_evaluate(double t, MeFrameData& frame)
+{
+	mcuCBHandle& mcu = mcuCBHandle::singleton();
+	if (_prevTime == 0)
+		_dt = 0.016;
+	else
+	{
+		_dt = t - _prevTime;
+		_prevTime = t;
+	}	
+	bool hasPhy = mcu.physicsEngine->getBoolAttribute("enable");
+#if 0
+	if (_valid && _context && hasPhy)
+	{
+		SbmPhysicsCharacter* phyChar = _character->getPhysicsCharacter();
+		std::vector<SbmJointObj*> jointObjList = phyChar->getJointObjList();
+		for (unsigned int i=0;i<jointObjList.size();i++)
+		{
+			SbmJointObj* obj = jointObjList[i];
+			SBJoint* joint = obj->getJoint();
+			if (!joint)	continue;
+			SrMat tran = obj->getRelativeOrientation();
+			//if (joint->getName() == "base" || joint->getName() == "world_offset")
+			//	continue;
+			std::string jname = joint->getName();
+			//if (joint->getParent())
+			//	jname = joint->getParent()->getName();
+
+			int channelId = _context->channels().search(jname, SkChannel::Quat);
+			if (channelId < 0)	continue;
+			int bufferId = frame.toBufferIndex(channelId);
+			if (bufferId < 0)	continue;			
+			SrQuat quat = SrQuat(tran);
+			frame.buffer()[bufferId + 0] = quat.w;
+			frame.buffer()[bufferId + 1] = quat.x;
+			frame.buffer()[bufferId + 2] = quat.y;
+			frame.buffer()[bufferId + 3] = quat.z;
+			
+			int positionChannelID = _context->channels().search(jname, SkChannel::XPos);
+			if (positionChannelID < 0) continue;
+			int posBufferID = frame.toBufferIndex(positionChannelID);
+			if (posBufferID < 0)	continue;
+			SrVec pos = tran.get_translation();
+			frame.buffer()[posBufferID + 0] = pos.x;
+			frame.buffer()[posBufferID + 1] = pos.y;
+			frame.buffer()[posBufferID + 2] = pos.z;			
+		}				
+	}
+#endif
+	return true;
+}
---
+++ core/smartbody/smartbody-lib/src/sbm/me_ct_physics_controller.h
@@ -0,0 +1,33 @@
+#ifndef _ME_CT_PHYSICS_CONTROLLER_H_
+#define _ME_CT_PHYSICS_CONTROLLER_H_
+
+#include <sbm/SBController.h>
+#include <sbm/sbm_character.hpp>
+#include <map>
+
+class MeCtPhysicsController : public SmartBody::SBController
+{
+	public:
+		static std::string CONTROLLER_TYPE;
+
+	private:
+		double			_prevTime;
+		double			_dt;
+		float 			_duration;
+		SkChannelArray	_channels;
+		SbmCharacter* 	_character;
+		bool			_valid;	
+	public:
+		MeCtPhysicsController(SbmCharacter* character);
+		~MeCtPhysicsController();
+
+		bool getValid()						{return _valid;}
+		void setValid(bool v)				{_valid = v;}		
+	private:
+		virtual bool controller_evaluate(double t, MeFrameData& frame);
+		virtual SkChannelArray& controller_channels()	{ return(_channels); }
+		virtual double controller_duration()			{ return((double)_duration); }
+		virtual const std::string& controller_type() const		{ return(CONTROLLER_TYPE); }
+};
+
+#endif
