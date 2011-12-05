21e441d32498e6b52aa038c8b26d08b6b358259d
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-12-05 19:20:37 +0000

    Added first pass of Panda3D renderer using C++

---
+++ trunk/core/Panda3dViewer/src/PandaRenderer.h
@@ -0,0 +1,35 @@
+#ifndef _PANDARENDERER_
+#define _PANDARENDERER_
+
+#include "vhcl.h"
+#include "bonebus.h"
+#include "vhmsg-tt.h"
+
+#include "SBListener.h"
+#include "asyncTask.h"
+#include "pandaFramework.h"
+#include "pandaSystem.h"
+
+class PandaRenderer : public AsyncTask
+{
+	public:
+		PandaRenderer(PandaFramework* framework, WindowFramework* window);
+		~PandaRenderer();
+
+		PandaFramework* getPandaFramework();
+		WindowFramework* getWindowFramework();
+		std::map<std::string, NodePath>& getCharacters();
+
+		virtual AsyncTask::DoneStatus do_task();
+		static void tt_client_callback( const char * op, const char * args, void * user_data );
+
+	protected:
+		SBListener* m_sbListener;
+		Smartbody_dll* m_sbm;
+		std::map<std::string, NodePath> m_characters;
+		WindowFramework* m_window;
+		PandaFramework* m_framework;
+};
+
+
+#endif
