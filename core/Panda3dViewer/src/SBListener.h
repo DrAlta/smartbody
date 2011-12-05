3fe62f77e6e8c8609808e3c99de64fb841c23b96
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2011-12-05 19:20:37 +0000

    Added first pass of Panda3D renderer using C++

---
+++ core/Panda3dViewer/src/SBListener.h
@@ -0,0 +1,27 @@
+#ifndef _SBLISTENER_H
+#define _SBLISTENER_H
+
+#include "vhcl.h"
+#include "smartbody-dll.h"
+#include <string>
+
+class PandaRenderer;
+
+class SBListener : public SmartbodyListener
+{
+	public:
+		SBListener(PandaRenderer* app);
+
+		virtual void OnCharacterCreate( const std::string & name );
+		virtual void OnCharacterCreate( const std::string & name, const std::string & objectClass );
+		virtual void OnCharacterDelete( const std::string & name );
+		virtual void OnCharacterChange( const std::string & name );
+
+	protected:
+		PandaRenderer* m_app;
+
+
+};
+
+
+#endif
---
+++ core/Panda3dViewer/src/main.cpp
@@ -0,0 +1,30 @@
+#include <vhcl.h>
+#include "pandaFramework.h"
+#include "pandaSystem.h"
+
+#include "PandaRenderer.h"
+
+ 
+PandaFramework framework;
+ 
+int main(int argc, char *argv[]) {
+	PandaFramework framework;
+    //open a new window framework
+  framework.open_framework(argc, argv);
+    //set the window title to My Panda3D Window
+  framework.set_window_title("My Panda3D Window");
+    //open the window
+  WindowFramework* window = framework.open_window();
+  window->setup_trackball();
+ 
+  PandaRenderer panda(&framework, window);
+
+  PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr(); 
+  taskMgr->add(&panda);
+ 
+    //do the main loop, equal to run() in python
+  framework.main_loop();
+    //close the window framework
+  framework.close_framework();
+  return (0);
+}
