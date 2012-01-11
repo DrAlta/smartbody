6dc14d96a9feb88810f9a8aa74342b2572c339eb
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-01-11 02:00:36 +0000

    Normalized 3D parameterized values in viewer. Tetrahedral blending with meters units can now be seen. Refactored some viewer code.

--- trunk/core/smartbody/sbm/src/panimationviewer/ParamAnimRunTimeEditor.h
+++ trunk/core/smartbody/sbm/src/panimationviewer/ParamAnimRunTimeEditor.h
@@ -25,107 +25,15 @@
 
 #include <FL/Fl_Slider.H>
 #include <vhcl.h>
-#include "PanimationWindow.h"
 #include <FL/Fl_Gl_Window.H>
 #include <FL/Fl_Hold_Browser.H>
 #include <sr/sr_gl.h>
 #include <sr/sr_light.h>
 #include <sr/sr_camera.h>
 #include <sr/sr_event.h>
+#include "PanimationWindow.h"
 
-class PanimationWindow;
 class ParameterGroup;
-
-class Parameter3DVisualization : public Fl_Gl_Window
-{
-	public:
-		Parameter3DVisualization(int x, int y, int w, int h, char* name, PAStateData* s, ParameterGroup* window);
-		~Parameter3DVisualization();
-
-		virtual void draw();
-		virtual int handle(int event);
-		virtual void resize(int x, int y, int w, int h);
-		void init_opengl();
-		void translate_event(SrEvent& e, SrEvent::EventType t, int w, int h, Parameter3DVisualization* viewer);
-		void mouse_event(SrEvent& e);
-
-		// user data
-		void drawTetrahedrons();
-		void drawGrid();
-		void drawParameter();
-
-	public:
-		SrCamera cam;
-		SrEvent e;
-		float gridSize;
-		float gridStep;
-		float floorHeight;
-
-	private:
-		PAStateData* state;
-		ParameterGroup* paramGroup;
-};
-
-class ParameterVisualization : public Fl_Group
-{
-	public:
-		ParameterVisualization(int x, int y, int w, int h, char* name, PAStateData* s, ParameterGroup* window);
-		~ParameterVisualization();
-
-		virtual void draw();
-		virtual int handle(int event);
-		virtual void setup();
-		virtual void resize(int x, int y, int w, int h);
-		void setSlider(int x, int y);
-		void getActualPixel(float paramX, float paramY, int& x, int& y);
-		void getActualParam(float& paramX, float& paramY, int x, int y);
-		void setPoint(int x, int y);
-
-	private:
-		void getBound(int ptX, int ptY, int& x, int& y, int& w, int& h);
-
-	private:
-		static const int pad = 5;
-		static const int margin = 10;
-		static const int gridSizeX = 50;
-		static const int gridSizeY = 30;
-		float scaleX;
-		float scaleY;
-		int centerX;
-		int centerY;
-		int width;
-		int height;
-		int paramX;
-		int paramY;
-		PAStateData* state;					// !!!Careful about this state, this PAState is a pointer to mcu state pool, not the current state to specific character
-		ParameterGroup* paramGroup;
-};
-
-class ParameterGroup : public Fl_Group
-{
-	public:
-		ParameterGroup(int x, int y, int w, int h, char* name, PAStateData* s, PanimationWindow* window, bool ex = false);
-		~ParameterGroup();
-
-		virtual void resize(int x, int y, int w, int h);
-		
-		static void updateXAxisValue(Fl_Widget* widget, void* data);
-		static void updateXYAxisValue(Fl_Widget* widget, void* data);
-		static void updateXYZAxisValue(Fl_Widget* widget, void* data);
-		void updateWeight();
-		PAStateData* getCurrentPAStateData();
-		SbmCharacter* getCurrentCharacter();
-
-	public:
-		PanimationWindow* paWindow;
-		PAStateData* state;
-		ParameterVisualization* paramVisualization;
-		Parameter3DVisualization* param3DVisualization;
-		bool exec;
-		Fl_Value_Slider* xAxis;
-		Fl_Value_Slider* yAxis;
-		Fl_Value_Slider* zAxis;
-};
 
 class PARunTimeEditor : public Fl_Group
 {
---
+++ trunk/core/smartbody/sbm/src/panimationviewer/Parameter3DVisualization.h
@@ -0,0 +1,41 @@
+#ifndef _PARAMETER3DVISUALIZATION_H_
+#define _PARAMETER3DVISUALIZATION_H_
+
+#include <FL/Fl_Group.h>
+#include <FL/Fl_Gl_Window.h>
+#include <sbm/me_ct_param_animation_data.h>
+#include "ParameterGroup.h"
+
+class Parameter3DVisualization : public Fl_Gl_Window
+{
+	public:
+		Parameter3DVisualization(int x, int y, int w, int h, char* name, PAStateData* s, ParameterGroup* window);
+		~Parameter3DVisualization();
+
+		virtual void draw();
+		virtual int handle(int event);
+		virtual void resize(int x, int y, int w, int h);
+		void init_opengl();
+		void translate_event(SrEvent& e, SrEvent::EventType t, int w, int h, Parameter3DVisualization* viewer);
+		void mouse_event(SrEvent& e);
+
+		// user data
+		void drawTetrahedrons();
+		void drawGrid();
+		void drawParameter();
+		SrVec determineScale();
+
+	public:
+		SrCamera cam;
+		SrEvent e;
+		float gridSize;
+		float gridStep;
+		float floorHeight;
+
+	private:
+		PAStateData* state;
+		ParameterGroup* paramGroup;
+		std::vector<SrVec> tet;
+};
+
+#endif
