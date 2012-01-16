49d158b15676ac7ad152bfa48c10f6d319b79e7e
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-01-16 07:44:43 +0000

    Moved parser .C files to .cpp for easier compilation. Fixed Android build (iOs build needs changes as well).

--- trunk/core/smartbody/smartbody-lib/src/sbm/Physics/SbmPhysicsSim.cpp
+++ trunk/core/smartbody/smartbody-lib/src/sbm/Physics/SbmPhysicsSim.cpp
@@ -1,7 +1,7 @@
 #include "SbmPhysicsSim.h"
 #include <sbm/mcontrol_util.h>
 #include <sr/sr_output.h>
-#include <boost/math/special_functions/fpclassify.hpp>
+//#include <boost/math/special_functions/fpclassify.hpp>
 
 SbmPhysicsSim::SbmPhysicsSim(void)
 {
