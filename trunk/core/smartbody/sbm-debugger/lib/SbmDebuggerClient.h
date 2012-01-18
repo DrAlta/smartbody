72b628f78793e61fa5047a27fe70d93bacdaf209
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-01-18 01:18:57 +0000

    sbm-debugger - initial library commit for the SBMDebugger

---
+++ trunk/core/smartbody/sbm-debugger/lib/SbmDebuggerClient.h
@@ -0,0 +1,36 @@
+
+#include <string>
+#include <vector>
+
+#include "SbmDebuggerCommon.h"
+
+
+class SbmDebuggerClient
+{
+private:
+   //vhmsg::Client m_vhmsg;
+   std::vector<std::string> m_processIdList;
+   std::string m_sbmId;
+   bool m_connectResult;
+
+
+   Scene m_scene;
+
+public:
+   SbmDebuggerClient();
+   virtual ~SbmDebuggerClient();
+
+   Scene* GetScene() { return &m_scene; }
+
+   void QuerySbmProcessIds();
+   const std::vector<std::string> & GetSbmProcessIds() const { return m_processIdList; }
+
+   void Connect(const std::string & id);
+   bool GetConnectResult() { return m_connectResult; }
+
+   void Init();
+   void Update();
+   void StartUpdates(double updateFrequencyS);
+
+   void ProcessVHMsgs(const char * op, const char * args);
+};
