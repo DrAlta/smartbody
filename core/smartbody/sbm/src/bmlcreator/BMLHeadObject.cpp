fb407d2be5161eff1c40b039f245415ce2a68afc
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-02-04 01:02:06 +0000

    Removed ORIENT from <head> BML in BML creator - not supported.

--- core/smartbody/sbm/src/bmlcreator/BMLHeadObject.cpp
+++ core/smartbody/sbm/src/bmlcreator/BMLHeadObject.cpp
@@ -9,33 +9,27 @@ BMLHeadObject::BMLHeadObject() : BMLObject()
 	headTypes.push_back("NOD");
 	headTypes.push_back("SHAKE");
 	headTypes.push_back("TOSS");
-	headTypes.push_back("ORIENT");
 	headTypes.push_back("WIGGLE");
 	headTypes.push_back("WAGGLE");
 	typeAttr->setValidValues(headTypes);
 
 	createDoubleAttribute("repeats", 1, true, "Basic", 30, false, false, false, "number of times a NOD or SHAKE repeats");
-	
-
-	createStringAttribute("direction", "", true, "Basic", 40, false, false, false, "?");
-	createStringAttribute("target", "", true, "Basic", 50, false, false, false, "?");
-	createDoubleAttribute("angle", 0, true, "Basic", 60, false, false, false, "?");
 
 
-	createDoubleAttribute("amount", 0, true, "Basic", 70, false, false, false, "?");
-	createDoubleAttribute("sbm:smooth", 0, true, "Basic", 80, false, false, false, "?");
-	createDoubleAttribute("sbm:period", 0, true, "Basic", 90, false, false, false, "?");
-	createDoubleAttribute("sbm:warp", 0, true, "Basic", 100, false, false, false, "?");
-	createDoubleAttribute("sbm:accel", 0, true, "Basic", 110, false, false, false, "?");
-	createDoubleAttribute("sbm:pitch", 0, true, "Basic", 120, false, false, false, "?");
-	createDoubleAttribute("sbm:decay", 0, true, "Basic", 130, false, false, false, "?");
+	createDoubleAttribute("amount", 0, true, "Basic", 70, false, false, false, "magnitude of head movement, from 0 to 1");
+	createDoubleAttribute("sbm:smooth", 0, true, "Basic", 80, false, false, false, "smoothing parameter when starting and finishing head movements");
+	createDoubleAttribute("sbm:period", 0, true, "Basic", 90, false, false, false, "period of nod cycle, default is .5");
+	createDoubleAttribute("sbm:warp", 0, true, "Basic", 100, false, false, false, "warp parameter for wiggle and waggle");
+	createDoubleAttribute("sbm:accel", 0, true, "Basic", 110, false, false, false, "acceleration parameter for wiggle and waggle");
+	createDoubleAttribute("sbm:pitch", 0, true, "Basic", 120, false, false, false, "pitch parameter for wiggle and waggle");
+	createDoubleAttribute("sbm:decay", 0, true, "Basic", 130, false, false, false, "decay parameter for wiggle and waggle");
 
 
-	createStringAttribute("start", "", true, "Basic", 150, false, false, false);
-	createStringAttribute("ready", "", true, "Basic", 160, false, false, false);
-	createStringAttribute("stroke", "", true, "Basic", 170, false, false, false);
-	createStringAttribute("relax", "", true, "Basic", 180, false, false, false);
-	createStringAttribute("end", "", true, "Basic", 190, false, false, false);
+	createStringAttribute("start", "", true, "Basic", 150, false, false, false, "when the head movements starts");
+	createStringAttribute("ready", "", true, "Basic", 160, false, false, false, "preparation phase of head movement"); 
+	createStringAttribute("stroke", "", true, "Basic", 170, false, false, false, "the mid-point of the head movement");
+	createStringAttribute("relax", "", true, "Basic", 180, false, false, false, "finishing phase of head movement");
+	createStringAttribute("end", "", true, "Basic", 190, false, false, false, "when the head movement ends");
 }
 
 BMLHeadObject::~BMLHeadObject()
