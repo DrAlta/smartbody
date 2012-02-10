5f6534d49bc61a30784010f682b763c82435b781
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-02-10 21:22:44 +0000

    Removed tools/examples folder

--- tools/examples/tutorialA/inc/lib_b.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIB_B_H
-#define LIB_B_H
-
-int lib_b_main( int argc, char **argv );
-
-#endif
--- tools/examples/tutorialA/lib/lib_b.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-
-int lib_b_main( int argc, char **argv )	{
-
-	printf( "Hello. lib B\n" );
-	return( 0 );
-}
