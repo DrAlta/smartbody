372fbea43f7c23985347b83772e8f6164e5e34f2
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-02-16 04:45:08 +0000

    smartbody-dll - removed old code path defined with USE_SBSCENE.  Moving forward with using the new scene API

--- core/smartbody/smartbody-dll/smartbody-dll.cpp
+++ core/smartbody/smartbody-dll/smartbody-dll.cpp
@@ -34,8 +34,6 @@ using std::string;
 #endif
 #endif
 
-#define USE_SBSCENE  1
-
 
 #ifdef WIN_BUILD
 BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved )
@@ -143,76 +141,46 @@ SMARTBODY_DLL_API Smartbody_dll::~Smartbody_dll()
 
 SMARTBODY_DLL_API void Smartbody_dll::SetSpeechAudiofileBasePath( const std::string & basePath )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
    // TODO: need a scene->clearAssetPath("audio");
    // TODO: need a scene->setPathPrefix(??);
 
-   mcu.audio_paths = srPathList();
-   mcu.audio_paths.setPathPrefix(mcu.getMediaPath());
-   scene->addAssetPath("audio", basePath);
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
    // clear the old audio path list
    mcu.audio_paths = srPathList();
    mcu.audio_paths.setPathPrefix(mcu.getMediaPath());
-   mcu.audio_paths.insert((char*) basePath.c_str());
-#endif
+   scene->addAssetPath("audio", basePath);
 }
 
 
 SMARTBODY_DLL_API void Smartbody_dll::SetFacebone( const bool enabled )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
    // TODO: need a scene->setFacebone(enabled);
    scene;
    mcu.net_face_bones = enabled;
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-   mcu.net_face_bones = enabled;
-#endif
 }
 
 
 SMARTBODY_DLL_API void Smartbody_dll::SetProcessId( const std::string & processId )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
    // TODO: need a scene->setProcessId(processId);
    scene;
    mcu.set_process_id( processId.c_str() );
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-   mcu.set_process_id( processId.c_str() );
-#endif
 }
 
 
 SMARTBODY_DLL_API void Smartbody_dll::SetMediaPath( const std::string & path )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
    scene->setMediaPath(path);
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-   mcu.setMediaPath( path );
-#endif
 }
 
 
@@ -222,18 +190,13 @@ SMARTBODY_DLL_API bool Smartbody_dll::Init(const std::string& pythonLibPath, boo
 
    XMLPlatformUtils::Initialize();  // Initialize Xerces before creating MCU
 
-#if USE_SBSCENE
+
    // TODO: Replace with SBScene * g_scene = new SBScene();
    mcuCBHandle & mcu = mcuCBHandle::singleton();
 
    // TODO: Replace with g_scene->SetCharacterListener(m_internalListener)
    mcu.sbm_character_listener = m_internalListener;
 
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-   mcu.sbm_character_listener = m_internalListener;
-#endif
 
    SetSpeechAudiofileBasePath( "../../" );
 
@@ -291,7 +254,6 @@ SMARTBODY_DLL_API void Smartbody_dll::SetListener( SmartbodyListener * listener 
 
 SMARTBODY_DLL_API bool Smartbody_dll::Update( const double timeInSeconds )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
@@ -300,16 +262,6 @@ SMARTBODY_DLL_API bool Smartbody_dll::Update( const double timeInSeconds )
    bool update_sim = mcu.update_timer( timeInSeconds );
    if( update_sim ) mcu.update();
    return true;
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-
-   bool update_sim = mcu.update_timer( timeInSeconds );
-   if( update_sim ) mcu.update();
-
-   return true;
-#endif
 }
 
 
@@ -349,49 +301,28 @@ SMARTBODY_DLL_API void Smartbody_dll::SetDebuggerRendererRightHanded( bool enabl
 
 SMARTBODY_DLL_API bool Smartbody_dll::ProcessVHMsgs( const char * op, const char * args )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
    string s = string(op) + string(" ") + string(args);
    scene->command( s.c_str() );
 
-   return true;
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-
-   mcu.execute( op, (char *)args );
-
-
-   mcu._scene->getDebuggerServer()->ProcessVHMsgs(op, args);
-
+   scene->getDebuggerServer()->ProcessVHMsgs(op, args);
 
    return true;
-#endif
 }
 
 
 SMARTBODY_DLL_API int Smartbody_dll::GetNumberOfCharacters()
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
    return scene->getNumCharacters();
-
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-
-   return mcu.getNumCharacters();
-#endif
 }
 
 
 SMARTBODY_DLL_API SmartbodyCharacter& Smartbody_dll::GetCharacter( const string & name )
 {
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
@@ -473,11 +404,11 @@ SMARTBODY_DLL_API SmartbodyCharacter& Smartbody_dll::GetCharacter( const string 
             SmartbodyJoint& joint = c.m_joints[i];
             joint.m_name = jointName;			
             SrQuat jointQ = j->quat()->value();
-            // 			if (i==136)
-            // 			{
-            // 				//sr_out << "eyeball quat = " << jointQ << srnl;
-            // 				LOG("eyeball quat = %f %f %f %f\n",jointQ.x,jointQ.y,jointQ.z,jointQ.w);
-            // 			}
+            // if (i==136)
+            // {
+            //    //sr_out << "eyeball quat = " << jointQ << srnl;
+            //    LOG("eyeball quat = %f %f %f %f\n",jointQ.x,jointQ.y,jointQ.z,jointQ.w);
+            // }
 
             joint.x = posx;
             joint.y = posy;
@@ -507,123 +438,6 @@ SMARTBODY_DLL_API SmartbodyCharacter& Smartbody_dll::GetCharacter( const string 
    {
       return m_emptyCharacter;
    }
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-
-   SbmCharacter * char_p = mcu.getCharacter(name );
-   if ( char_p )
-   {
-      std::map<std::string,SmartbodyCharacter*>::iterator mi = m_characters.find(name);
-      SmartbodyCharacter* pc = NULL;
-      bool hasChar = false;
-      if (mi != m_characters.end())
-      {
-         pc = mi->second;
-         hasChar = true;
-      }
-      else
-      {
-         pc = new SmartbodyCharacter();
-         hasChar = false;
-         m_characters[name] = pc;
-      }
-
-      SmartbodyCharacter& c = *pc;
-
-      const SkJoint * joint = char_p->get_world_offset_joint();
-
-      const SkJointPos * pos = joint->const_pos();
-      float x = pos->value( SkJointPos::X );
-      float y = pos->value( SkJointPos::Y );
-      float z = pos->value( SkJointPos::Z );
-
-      SkJoint::RotType rot_type = joint->rot_type();
-      if ( rot_type != SkJoint::TypeQuat )
-      {
-         //cerr << "ERROR: Unsupported world_offset rotation type: " << rot_type << " (Expected TypeQuat, "<<SkJoint::TypeQuat<<")"<<endl;
-      }
-
-      // const_cast because the SrQuat does validation (no const version of value())
-      const SrQuat & q = ((SkJoint *)joint)->quat()->value();
-
-      c.m_name = char_p->getName();
-      c.x = x;
-      c.y = y;
-      c.z = z;
-      c.rw = q.w;
-      c.rx = q.x;
-      c.ry = q.y;
-      c.rz = q.z;
-
-
-      const std::vector<SkJoint *> & joints  = char_p->getSkeleton()->joints();
-
-      for ( size_t i = 0; i < joints.size(); i++ )
-      {
-         // const_cast because the SrQuat does validation (no const version of value())
-         SkJoint * j = joints[i];
-
-         SrQuat q = j->quat()->value();
-
-         //printf( "%s %f %f %f %f\n", (const char *)j->name(), q.w, q.x, q.y, q.z );
-
-         float posx = j->pos()->value( 0 );
-         float posy = j->pos()->value( 1 );
-         float posz = j->pos()->value( 2 );
-         if ( false )
-         {
-            posx += j->offset().x;
-            posy += j->offset().y;
-            posz += j->offset().z;
-         }
-
-         std::string jointName;
-         if (j->extName() != "")
-            jointName = j->extName();
-         else
-            jointName = j->name();
-
-         if (hasChar)
-         {
-            SmartbodyJoint& joint = c.m_joints[i];
-            joint.m_name = jointName;			
-            SrQuat jointQ = j->quat()->value();
-            // 			if (i==136)
-            // 			{
-            // 				//sr_out << "eyeball quat = " << jointQ << srnl;
-            // 				LOG("eyeball quat = %f %f %f %f\n",jointQ.x,jointQ.y,jointQ.z,jointQ.w);
-            // 			}
-
-            joint.x = posx;
-            joint.y = posy;
-            joint.z = posz;
-            joint.rw = q.w;
-            joint.rx = q.x;
-            joint.ry = q.y;
-            joint.rz = q.z;
-         }
-         else
-         {
-            SmartbodyJoint joint;
-            joint.m_name = jointName;
-            joint.x = posx;
-            joint.y = posy;
-            joint.z = posz;
-            joint.rw = q.w;
-            joint.rx = q.x;
-            joint.ry = q.y;
-            joint.rz = q.z;
-            c.m_joints.push_back( joint );
-         }
-      }
-      return c;
-   }
-   else
-   {
-      return m_emptyCharacter;
-   }
-#endif
 }
 
 
@@ -631,7 +445,6 @@ bool Smartbody_dll::InitVHMsg()
 {
 #if !defined(__ANDROID__) && !defined(SBM_IPHONE)
 
-#if USE_SBSCENE
    mcuCBHandle & mcu = mcuCBHandle::singleton();
    SBScene * scene = mcu._scene;
 
@@ -643,17 +456,6 @@ bool Smartbody_dll::InitVHMsg()
    if (err == vhmsg::TTU_SUCCESS)
       mcu.vhmsg_enabled = true;
 
-
-#else
-   mcuCBHandle & mcu = mcuCBHandle::singleton();
-
-   printf( "Starting VHMsg (DLL side)\n" );
-
-   int err = vhmsg::ttu_open();
-   if (err == vhmsg::TTU_SUCCESS)
-      mcu.vhmsg_enabled = true;
-#endif
-
 #endif
    return true;
 }
@@ -662,5 +464,4 @@ bool Smartbody_dll::InitVHMsg()
 void Smartbody_dll::RegisterCallbacks()
 {
   // place any additional commands here...
-
 }
