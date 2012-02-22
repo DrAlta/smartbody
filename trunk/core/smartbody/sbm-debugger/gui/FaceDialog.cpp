bed8d582eb244cd95dbb60b5e61daac467733a4e
adamreilly <adamreilly@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-02-22 22:34:15 +0000

    sbm debugger- added functionality to send and receive python commands to and from smartbody. The face viewer window is now dynamically populated with facial expressions using this method

--- trunk/core/smartbody/sbm-debugger/gui/FaceDialog.cpp
+++ trunk/core/smartbody/sbm-debugger/gui/FaceDialog.cpp
@@ -5,16 +5,20 @@
 
 #define SPIN_BOX "spinBox"
 
-FaceDialog::FaceDialog(Scene* pScene, QWidget* parent ) : QDialog(parent)
+// callbacks
+bool GetVisemeNames(void* caller, NetRequest* req);
+bool GetAUNumbers(void* caller, NetRequest* req);
+//bool GetNumVisemes(void* caller, NetRequest* req);
+
+
+FaceDialog::FaceDialog(SbmDebuggerClient* client, QWidget* parent ) : QDialog(parent)
 {
-   m_pScene = pScene;
+   m_client = client;
+   m_pScene = m_client->GetScene();;
    ui.setupUi(this);
    QVBoxLayout *scrollViewLayout = new QVBoxLayout;
    ui.scrollArea->setLayout(scrollViewLayout);
-
-   AddFacialExpression("au_1_left", 0);
-   AddFacialExpression("au_1_right", 0);
-   AddFacialExpression("OO", 0);
+   ui.scrollArea->widget()->setLayout(scrollViewLayout);
 
    ui.characterNameBox->clear();
    for (unsigned int i = 0; i < m_pScene->m_characters.size(); i++)
@@ -23,6 +27,12 @@ FaceDialog::FaceDialog(Scene* pScene, QWidget* parent ) : QDialog(parent)
    }
 
    connect(ui.resetButton, SIGNAL(pressed()), this, SLOT(Reset()));
+   connect(ui.characterNameBox, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(CharacterSelectionChanged(const QString&)));
+
+   if (m_pScene->m_characters.size() >= 1)
+   {
+      CharacterSelectionChanged(m_pScene->m_characters[0].m_name.c_str());
+   }
 }
 
 FaceDialog::~FaceDialog()
@@ -64,10 +74,17 @@ void FaceDialog::AddFacialExpression(const string& name, double weight)
    frame->setLayout(frameLayout);
 
    // add the frame to the scroll view
-   ui.scrollArea->layout()->addWidget(frame);
+   ui.scrollArea->widget()->layout()->addWidget(frame);
 
    // setup gui messages
    connect(weightSlider, SIGNAL(valueChanged(int)), this, SLOT(SliderValueChanged(int)));
+
+
+   // save for later deletion
+   m_scrollListChildren.push_back(expressionName);
+   m_scrollListChildren.push_back(weightSlider);
+   m_scrollListChildren.push_back(weightDisplay);
+   m_scrollListChildren.push_back(frame);
 }
 
 void FaceDialog::Reset()
@@ -76,6 +93,23 @@ void FaceDialog::Reset()
    {
       m_Sliders[i]->setValue(0);
    }
+}
+
+void FaceDialog::CharacterSelectionChanged(const QString& selection)
+{
+   // get AU's
+   string commandAU = vhcl::Format("scene.getCharacter(\'%s\').getFaceDefinition().getAUNumbers()", selection.toStdString().c_str());
+   m_client->SendSBMCommand(NetRequest::Get_Viseme_Names, "int-array", commandAU, GetAUNumbers, this);
+
+   // get visemes
+   string commandViseme = vhcl::Format("scene.getCharacter(\'%s\').getFaceDefinition().getVisemeNames()", selection.toStdString().c_str());
+   m_client->SendSBMCommand(NetRequest::Get_Viseme_Names, "string-array", commandViseme, GetVisemeNames, this);
+
+   for (unsigned int i = 0; i < m_scrollListChildren.size(); i++)
+   {
+      delete m_scrollListChildren[i];
+   }
+   m_scrollListChildren.clear(); 
 }
 
 void FaceDialog::SliderValueChanged(int val)
@@ -94,7 +128,6 @@ void FaceDialog::SliderValueChanged(int val)
    spinBox->setValue(weight);
 
    // inform smartbody
-   
    if (visemeName.contains("au"))
    {
       QStringList split = visemeName.split("_");
@@ -103,6 +136,13 @@ void FaceDialog::SliderValueChanged(int val)
          string msg = vhcl::Format("sbm test bml char %s <face type=\"facs\" %s=\"%s\" side=\"%s\" amount=\"%.2f\" />",
             ui.characterNameBox->currentText().toStdString().c_str(), split[0].toStdString().c_str(),
             split[1].toStdString().c_str(), split[2].toStdString().c_str(), weight); 
+         vhmsg::ttu_notify1(msg.c_str());
+      }
+      else if (split.length() == 2)
+      {
+         string msg = vhcl::Format("sbm test bml char %s <face type=\"facs\" %s=\"%s\" amount=\"%.2f\" />",
+            ui.characterNameBox->currentText().toStdString().c_str(), split[0].toStdString().c_str(),
+            split[1].toStdString().c_str(), weight); 
          vhmsg::ttu_notify1(msg.c_str());
       }
       else
@@ -115,6 +155,32 @@ void FaceDialog::SliderValueChanged(int val)
       string msg = vhcl::Format("sbm char %s viseme %s %.2f 1",
          ui.characterNameBox->currentText().toStdString().c_str(), visemeName.toStdString().c_str(), weight); 
       vhmsg::ttu_notify1(msg.c_str());
-
    }
+}
+
+bool GetVisemeNames(void* caller, NetRequest* req)
+{
+   FaceDialog* dlg = req->getCaller<FaceDialog*>();
+   
+   vector<string> visemeNames = req->Args();
+   for (unsigned int i = 0; i < visemeNames.size(); i++)
+   {
+      dlg->AddFacialExpression(visemeNames[i].c_str(), 0);
+   }
+
+   return true;
+}
+
+bool GetAUNumbers(void* caller, NetRequest* req)
+{
+   FaceDialog* dlg = req->getCaller<FaceDialog*>();
+   
+   vector<string> auNums = req->Args();
+   for (unsigned int i = 0; i < auNums.size(); i++)
+   {
+      auNums[i].insert(0, "au_");
+      dlg->AddFacialExpression(auNums[i].c_str(), 0);
+   }
+
+   return true;
 }
