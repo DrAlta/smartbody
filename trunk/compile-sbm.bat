31ab42b36a25cf7b3b06c060e7c766493686bf5b
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-03-27 02:01:23 +0000

    build - switch the build server to run vs2010 version

--- trunk/compile-sbm.bat
+++ trunk/compile-sbm.bat
@@ -1,11 +1,11 @@
 
 setlocal
 
-call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 @rem vcvarsall turns echo off and doesn't turn it back on, shame on them
 @echo on
 
-devenv vs2008.sln /build Debug
-devenv vs2008.sln /build Release
+devenv vs2010.sln /build Debug
+devenv vs2010.sln /build Release
 
 endlocal
