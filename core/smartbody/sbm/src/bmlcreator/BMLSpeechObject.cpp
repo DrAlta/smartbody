bbf5fb767f3312684c666ea4010b783456e0083a
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-03-30 18:50:19 +0000

    Fix for OSx - FLTK doesn't like slashes in choice items, so commented out speech type in BML Creator on OSx.

--- core/smartbody/sbm/src/bmlcreator/BMLSpeechObject.cpp
+++ core/smartbody/sbm/src/bmlcreator/BMLSpeechObject.cpp
@@ -6,10 +6,12 @@ BMLSpeechObject::BMLSpeechObject() : BMLObject()
 	setName("speech");
 
 	typeAttr = createStringAttribute("type", "text/plain", "", "Basic", 10, false, false, false, "Format of speech content.");
+#ifndef __APPLE__
 	std::vector<std::string> types;
 	types.push_back("text/plain");
 	types.push_back("application/ssml+xml");
 	typeAttr->setValidValues(types);
+#endif
 
 	refAttr = createStringAttribute("ref", "", "", "Basic", 100, false, false, false, "Reference .xml file that contains speech information. Either set this attribute, or enter the content.");
 	contentAttr = createStringAttribute("content", "", "", "Basic", 100, false, false, false, "Content of speech, such as 'hello, my name is John'. When using ssml, tags (such as <emphasis>) can be used.");
