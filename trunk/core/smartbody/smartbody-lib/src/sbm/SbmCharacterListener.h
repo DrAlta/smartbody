0e41c907b389cda08cce9341b51e8f5c4fe3a8b9
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-04-10 18:04:41 +0000

    Add missing file from last checkin.

---
+++ trunk/core/smartbody/smartbody-lib/src/sbm/SbmCharacterListener.h
@@ -0,0 +1,20 @@
+#ifndef  __SBM_CHARACTER_LISTENER__
+#define  __SBM_CHARACTER_LISTENER__
+#include <string>
+
+// This class is meant for listening to specific events that could be handled externally from smartbody
+// Currently being used by smartbody-dll
+class SBMCharacterListener
+{
+public:
+	virtual void OnCharacterCreate( const std::string & name, const std::string & objectClass ) {}
+	virtual void OnCharacterDelete( const std::string & name ) {}
+	virtual void OnCharacterUpdate( const std::string & name, const std::string & objectClass ) {}
+	virtual void OnCharacterChanged( const std::string& name ) {}
+	virtual void OnCharacterChangeMesh( const std::string& name ) {}
+	virtual void OnPawnCreate( const std::string & name ) {}
+	virtual void OnPawnDelete( const std::string & name ) {}
+	virtual void OnViseme( const std::string & name, const std::string & visemeName, const float weight, const float blendTime ) {}
+	virtual void OnChannel( const std::string & name, const std::string & channelName, const float value) {}
+};
+#endif
