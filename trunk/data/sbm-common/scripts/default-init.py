c6974ecb002b6f58868ae354ae7222e49a0c261b
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-04-14 00:01:47 +0000

    Added reach setup to default configuration.

--- trunk/data/sbm-common/scripts/default-init.py
+++ trunk/data/sbm-common/scripts/default-init.py
@@ -5,15 +5,12 @@ print "|--------------------------------------------|"
 scene.run("default-viewer.py")
 
 ### Load data/sbm-common assets
-### Assumes current directory is: core/smartbody/sbm/bin
 scene.addAssetPath("seq", "../../../../data/sbm-common/scripts")
 scene.addAssetPath("seq", "../../../../data/sbm-test/scripts")
 scene.addAssetPath("mesh", "../../../../data/mesh")
 
 scene.run("init-common-assets.py")
-#0	seq init-general-parameters
 scene.run("init-common-face.py")
-scene.command("panim enable")
 
 doctor = scene.createCharacter("doctor", "SasoBase.SasoDoctorPerez")
 doctorSkeleton = scene.createSkeleton("common.sk")
@@ -27,7 +24,7 @@ doctor.setVoice("remote")
 doctor.setVoiceCode("Festival_voice_rab_diphone")
 doctor.createStandardControllers()
 doctor.setStringAttribute("deformableMesh", "doctor")
-'''
+
 elder = scene.createCharacter("elder", "SasoBase.Mayor")
 elderSkeleton = scene.createSkeleton("common.sk")
 elder.setSkeleton(elderSkeleton)
@@ -53,30 +50,29 @@ brad.setVoice("remote")
 brad.setVoiceCode("Festival_voice_rab_diphone")
 brad.createStandardControllers()
 brad.setStringAttribute("deformableMesh", "brad")
-'''
-# utah = scene.createCharacter("utah", "")
-# utahSkeleton = scene.createSkeleton("test_utah.sk")
-# utah.setSkeleton(utahSkeleton)
-# utah.setFaceDefinition(defaultFace)
-# utahPos = SrVec(-135, 0, 0)
-# utah.setPosition(utahPos)
-# utahHPR = SrVec(-17, 0, 0)
-# utah.setHPR(utahHPR)
-# utah.setVoice("remote")
-# utah.setVoiceCode("Festival_voice_rab_diphone")
-# utah.createStandardControllers()
-# utah.setStringAttribute("deformableMesh", "utah")
+
+utah = scene.createCharacter("utah", "")
+utahSkeleton = scene.createSkeleton("test_utah.sk")
+utah.setSkeleton(utahSkeleton)
+utah.setFaceDefinition(defaultFace)
+utahPos = SrVec(-135, 0, 0)
+utah.setPosition(utahPos)
+utahHPR = SrVec(-17, 0, 0)
+utah.setHPR(utahHPR)
+utah.setVoice("remote")
+utah.setVoiceCode("Festival_voice_rab_diphone")
+utah.createStandardControllers()
+utah.setStringAttribute("deformableMesh", "utah")
 
 scene.setDefaultCharacter("doctor")
 scene.setDefaultRecipient("elder")
 
 scene.run("init-param-animation.py")
-
 scene.run("init-steer-agents.py")
 
 scene.setBoolAttribute("internalAudio", True)
 
-
+# add the reaching database
 scene.run("init-example-reach.py")
 names = scene.getCharacterNames()
 for n in range(0, len(names)):
@@ -88,13 +84,12 @@ sim.start()
 bml.execBML('doctor', '<body posture="LHandOnHip_Motex"/>')
 bml.execBML('elder', '<body posture="LHandOnHip_Motex"/>')
 bml.execBML('brad', '<body posture="HandsAtSide_Motex"/>')
-bml.execBML('utah', '<body posture="HandsAtSide_Motex"/>')
+bml.execBML('utah', '<body posture="ChrUtah_Idle003"/>')
 
 bml.execBML('doctor', '<saccade mode="listen"/>')
 bml.execBML('elder', '<saccade mode="listen"/>')
 bml.execBML('brad', '<saccade mode="listen"/>')
 bml.execBML('utah', '<saccade mode="listen"/>')
-
 
 sim.resume()
 
