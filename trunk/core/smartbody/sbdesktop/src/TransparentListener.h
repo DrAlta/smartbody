093c60c20750a9c179736f4afcb60bc051c33529
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-04-30 17:57:35 +0000

    Added sbdesktop project: places a transparent SmartBody window on your desktop. Uses the same command line parameters as sbm-fltk, with the addition of -x, -y, -w, -h for desktop position and size, as in:
    sbdesktop -w 320 -h 240 -x 700 -y 500 -script default-init.py -scriptpath ../../../../data/sbm-common/scripts
    
    Currently only uses CPU-based deformable geometry (having trouble getting GPU rendering working).

---
+++ trunk/core/smartbody/sbdesktop/src/TransparentListener.h
@@ -0,0 +1,26 @@
+#ifndef _TRANSPARENTLISTENER_H_
+#define _TRANSPARENTLISTENER_H_
+
+#include <sbm/mcontrol_util.h>
+#include <sk/sk_scene.h>
+
+class TransparentListener : public SBMCharacterListener, public SmartBody::SBObserver
+{
+   public:
+	  TransparentListener();
+	  ~TransparentListener();
+
+      virtual void OnCharacterCreate( const std::string & name, const std::string & objectClass );
+      virtual void OnCharacterDelete( const std::string & name );
+	  virtual void OnCharacterUpdate( const std::string & name, const std::string & objectClass );
+	  virtual void OnCharacterChanged( const std::string& name );
+      virtual void OnPawnCreate( const std::string & name );
+      virtual void OnPawnDelete( const std::string & name );
+      virtual void OnViseme( const std::string & name, const std::string & visemeName, const float weight, const float blendTime );
+	  virtual void OnChannel( const std::string & name, const std::string & channelName, const float value);
+	  virtual void OnReset();
+
+	  virtual void notify(SmartBody::SBSubject* subject);
+};
+
+#endif
