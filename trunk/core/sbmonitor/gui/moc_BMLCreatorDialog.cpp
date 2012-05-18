6dcf2959a5103176036185c76a015265103cc177
adamreilly <adamreilly@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-05-18 21:10:07 +0000

    sbmonitor- implemented VH-428: Connect single option that connects to the first found sbm process

--- trunk/core/sbmonitor/gui/moc_BMLCreatorDialog.cpp
+++ trunk/core/sbmonitor/gui/moc_BMLCreatorDialog.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'BMLCreatorDialog.h'
 **
-** Created: Thu May 17 14:30:11 2012
+** Created: Fri May 18 13:45:41 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
