cbb5c76cd52f1db8382f7f4226053789bde300ff
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-05-30 21:09:26 +0000

    - transform all the phonemes to lower case to avoid possible case differences. - Fix some bugs in VisemeViewerWindow

--- core/smartbody/smartbody-lib/src/sb/SBPhonemeManager.cpp
+++ core/smartbody/smartbody-lib/src/sb/SBPhonemeManager.cpp
@@ -1,5 +1,6 @@
 #include <sb/SBPhonemeManager.h>
 #include <sb/SBPhoneme.h>
+#include <algorithm>
 #include <vhcl.h>
 
 namespace SmartBody {
@@ -25,14 +26,18 @@ SBDiphoneManager::~SBDiphoneManager()
 
 SBDiphone* SBDiphoneManager::createDiphone(const std::string& fromPhoneme, const std::string& toPhoneme, const std::string& name)
 {
-	SBDiphone* diphone = getDiphone(fromPhoneme, toPhoneme, name);
+	std::string lowerCaseFromPhoneme = fromPhoneme;
+	std::string lowerCaseToPhoneme = toPhoneme;
+	std::transform(fromPhoneme.begin(), fromPhoneme.end(), lowerCaseFromPhoneme.begin(), ::tolower);
+	std::transform(toPhoneme.begin(), toPhoneme.end(), lowerCaseToPhoneme.begin(), ::tolower);
+	SBDiphone* diphone = getDiphone(lowerCaseFromPhoneme, lowerCaseToPhoneme, name);
 	if (diphone)
 	{
-		LOG("Diphone set %s already contain diphone pair %s to %s, return existing one.", name.c_str(), fromPhoneme.c_str(), toPhoneme.c_str());
+		LOG("Diphone set %s already contain diphone pair %s to %s, return existing one.", name.c_str(), lowerCaseFromPhoneme.c_str(), lowerCaseToPhoneme.c_str());
 	}
 	else
 	{
-		diphone = new SBDiphone(fromPhoneme, toPhoneme);
+		diphone = new SBDiphone(lowerCaseFromPhoneme, lowerCaseToPhoneme);
 		_diphoneMap[name].push_back(diphone);
 	}
 	return diphone;
@@ -51,10 +56,14 @@ std::vector<SBDiphone*>& SBDiphoneManager::getDiphones(const std::string& name)
 
 SBDiphone* SBDiphoneManager::getDiphone(const std::string& fromPhoneme, const std::string& toPhoneme, const std::string& name)
 {
+	std::string lowerCaseFromPhoneme = fromPhoneme;
+	std::string lowerCaseToPhoneme = toPhoneme;
+	std::transform(fromPhoneme.begin(), fromPhoneme.end(), lowerCaseFromPhoneme.begin(), ::tolower);
+	std::transform(toPhoneme.begin(), toPhoneme.end(), lowerCaseToPhoneme.begin(), ::tolower);
 	std::vector<SBDiphone*>& diphones = getDiphones(name);
 	for (size_t i = 0; i < diphones.size(); i++)
 	{
-		if (diphones[i]->getFromPhonemeName() == fromPhoneme && diphones[i]->getToPhonemeName() == toPhoneme)
+		if (diphones[i]->getFromPhonemeName() == lowerCaseFromPhoneme && diphones[i]->getToPhonemeName() == lowerCaseToPhoneme)
 		{
 			return diphones[i];
 		}
