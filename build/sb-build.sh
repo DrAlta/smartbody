fdc3d3d55daaa222e7a27d1ae888b6b356116f8f
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-06-13 05:30:58 +0000

    build - rewrote automated build script in python.  Now works for mac and windows build servers.

---
+++ build/sb-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python sb-automated-build.py build password -mac-ci
---
+++ build/sb-checkCI.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python sb-automated-build.py checktime https://smartbody.svn.sourceforge.net/svnroot/smartbody/trunk user password 200 d: d:/VHToolkit-Builds -mac-ci
