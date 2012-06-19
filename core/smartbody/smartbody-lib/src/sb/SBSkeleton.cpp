c0259ed87034ada67233053e6c5858e779263f4c
hhung516 <hhung516@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-06-19 18:54:15 +0000

    part of the preRotation fixing/re-orienting code, more to come...
    testSkeleton.orientJointsLocalAxesToWorld() # this will re-orient joint local axes to match world coordinate, see function comment for details
    
    newSkeleton = testSkeleton.createSkelWithoutPreRot("newSkeleton.sk") # this will create a new world-aligned skeleton (standard T-pose) with no preRotations
    
    -David Huang
    2012-06-19

--- core/smartbody/smartbody-lib/src/sb/SBSkeleton.cpp
+++ core/smartbody/smartbody-lib/src/sb/SBSkeleton.cpp
@@ -3,6 +3,8 @@
 #include "SBCharacter.h"
 #include <sbm/mcontrol_util.h>
 
+#include <sr/sr_string.h>
+
 namespace SmartBody {
 
 SBSkeleton::SBSkeleton() : SkSkeleton()
@@ -207,4 +209,73 @@ void SBSkeleton::update()
 	}
 }
 
-};
+
+/* the following are designed to re-orient joints local axes. added by David Huang Jun 2012*/
+/* Orient skeleton joints local axes to match world coordinate axes (Y-up Z-front)
+// put skel into T-pose before running this! */
+void SBSkeleton::orientJointsLocalAxesToWorld(void)
+{
+	const std::vector<SkJoint*>& jnts = SkSkeleton::joints();
+	update_global_matrices();
+	for(size_t i=0; i<jnts.size(); i++)
+	{
+		SkJoint* j = jnts[i];
+		j->quat()->orientation(/*j->prerot() * */j->quat()->value());
+		const SrMat& gmat = j->gmat();
+		SrQuat grot; grot.set(gmat);
+		j->quat()->prerot(grot.inverse());
+		j->quat()->postrot(grot.inverse());
+	}
+	
+	// now we can remove local rotations
+	for(size_t i=0; i<jnts.size(); i++)
+		jnts[i]->init_rot();
+	update_global_matrices();
+}
+
+/* Create a new standard T-pose skel with no pre-rotations from source (TposeSk)
+// put source skel(TposeSk) into T-pose before running this!   */
+void SBSkeleton::_createSkelWithoutPreRot(SBSkeleton* TposeSk, SBSkeleton* newSk, const char* new_name)
+{
+	TposeSk->update_global_matrices();
+	newSk->copy(TposeSk); // first copy
+
+	if(new_name || strlen(new_name)<1)
+	{
+		newSk->name(string(new_name));
+		SrString fname(TposeSk->skfilename().c_str());
+		fname.remove_file_name();
+		fname.append(new_name); 
+		//fname.append(".sk");
+		newSk->skfilename(fname);
+	}
+	const std::vector<SkJoint*>& new_jnts = newSk->joints();
+	for(size_t i=1; i<new_jnts.size(); i++)
+	{
+		SkJoint* new_j = new_jnts[i];
+		if(!new_j->parent()) continue;
+		SkJoint* src_j = TposeSk->search_joint(new_j->name().c_str());
+		if(!src_j) continue;
+		if(!src_j->parent()) continue;
+		SrVec new_offset = src_j->gmat().get_translation() -
+			src_j->parent()->gmat().get_translation();
+		new_j->offset(new_offset);
+	}
+	for(size_t i=1; i<new_jnts.size(); i++)
+	{
+		SkJoint* new_j = new_jnts[i];
+		new_j->init_values();
+		new_j->quat()->prerot(SrQuat::null);
+		new_j->quat()->postrot(SrQuat::null);
+	}
+}
+// same as above but for Python interface
+SBSkeleton* SBSkeleton::createSkelWithoutPreRot(const char* new_name)
+{
+	SBSkeleton* newSk = new SBSkeleton;
+	_createSkelWithoutPreRot(this, newSk, new_name);
+	return newSk;
+}
+
+
+}; //namespace
