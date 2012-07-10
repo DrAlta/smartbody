ef2b6ad74140803b18b4a87690473bb7844d7113
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-07-10 23:22:23 +0000

    - Expose Charniak is Initialized function - Fix NVBG, now multiply NVBG instances can be initialized. - Fix the reset NVBG script. - Fix the NVBG path issue.

--- data/sbm-common/scripts/resetNVBG.py
+++ data/sbm-common/scripts/resetNVBG.py
@@ -2,13 +2,22 @@
 Reset NVBG, This script is used for debugging
 """
 
+print 'Resetting NVBG...'
+
 ''' Reset NVBG.py '''
 reload(NewNVBG.NVBG)
-from NewNVBG.NVBG import *	# why simply reload doesn't work???
+from NewNVBG.NVBG import *  
 
 ''' Reset Pyke '''
-from pyke import knowledge_engine
-import NewNVBG.nvbg
-import NewNVBG.brainstem
+reload(NewNVBG.nvbg)
+reload(NewNVBG.brainstem)
 nvbg_engine = knowledge_engine.engine(NewNVBG.nvbg)
 brainstem_engine = knowledge_engine.engine(NewNVBG.brainstem)
+
+characterNames = scene.getCharacterNames()
+numCharacters = len(characterNames)
+parser = getScene().getParser()
+for i in range(0, numCharacters):
+    n = NVBG(characterNames[i], getScene())
+    n.setParser(parser)
+    scene.getCharacter(characterNames[i]).getNvbg().nvbg = n
