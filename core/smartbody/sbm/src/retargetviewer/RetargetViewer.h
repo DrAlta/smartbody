38ca5a5b2512c5598de4242a155871963e2bcf59
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-07-19 23:04:36 +0000

    Added retarget viewer for beahvior sets.

---
+++ core/smartbody/sbm/src/retargetviewer/RetargetViewer.h
@@ -0,0 +1,31 @@
+#ifndef _RETARGETVIEWER_
+#define _RETARGETVIEWER_
+
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_Scroll.H>
+
+class RetargetViewer : public Fl_Double_Window
+{
+	public:
+		RetargetViewer(int x, int y, int w, int h, char* name);
+		~RetargetViewer();
+
+		void setCharacterName(const std::string& name);
+		void setSkeletonName(const std::string& name);
+		const std::string& getCharacterName();
+		const std::string& getSkeletonName();
+
+		static void RetargetCB(Fl_Widget* widget, void* data);
+		static void CancelCB(Fl_Widget* widget, void* data);
+
+	protected:
+		std::string _charName;
+		std::string _skelName;
+
+		Fl_Choice* _choiceCharacters;
+		Fl_Choice* _choiceSkeletons;
+		Fl_Scroll* _scrollGroup;
+
+};
+#endif
