0dc76556ef0b1e7a5bb5dc93b231576a9e91c319
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-10-01 22:44:09 +0000

    build - merging changes from toolkit build script.  Adding build types 'incremental' and 'fresh' to help with build times

--- trunk/build/sb-checkCI.sh
+++ trunk/build/sb-checkCI.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python sb-automated-build.py checktime https://smartbody.svn.sourceforge.net/svnroot/smartbody/trunk user password 200 d: d:/VHToolkit-Builds -mac-ci
+python sb-automated-build.py checktime https://smartbody.svn.sourceforge.net/svnroot/smartbody/trunk user password 200 d: d:/VHToolkit-Builds -mac-ci 72
--- trunk/build/sb-compile.sh
+++ trunk/build/sb-compile.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
 
-cd build/lib/festival/speech_tools
+cd lib/festival/speech_tools
 sed -i '' -e 's/-Wall/-Wall -arch i386/g' config/compilers/gcc_defaults.mak
 chmod 700 configure
 ./configure
 make clean
 make
 make install
-cd ../../../..
+cd ../../..
 
-cd build/lib/festival/festival
+cd lib/festival/festival
 chmod 700 configure
 ./configure
 make clean
 make
 make install
-cd ../../../..
+cd ../../..
 
 
-mkdir build/bin
-cd build/bin
+mkdir bin
+cd bin
 cmake -DCMAKE_OSX_ARCHITECTURES="i386" ..
 make
 make install
+cd ..
