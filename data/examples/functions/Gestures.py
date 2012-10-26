adcf64f884723274ffdde5ab4b247e6260a045ba
pzyu <pzyu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-10-26 00:13:20 +0000

    Fixed and updated some of the scripts

--- data/examples/functions/Gestures.py
+++ data/examples/functions/Gestures.py
@@ -2,14 +2,23 @@ def initGestureMap():
 	gMapManager = getScene().getGestureMapManager()
 	for name in scene.getCharacterNames():
 		gMap = gMapManager.createGestureMap(name)
-		# gMap.addGestureMapping(animationName, type, lexeme, posture, hand)
-		gMap.addGestureMapping('HandsAtSide_Arms_Sweep', '', 'SWEEP', 'HandsAtSide_Motex', 'BOTH_HANDS', '')
-		gMap.addGestureMapping('HandsAtSide_RArm_GestureYou', '', 'YOU', 'LHandsAtSide_Motex', 'RIGHT_HAND', '')
-		gMap.addGestureMapping('LHandOnHip_Arms_GestureWhy', '', 'WHY', 'LHandOnHip_Motex', 'BOTH_HANDS', '')
-		gMap.addGestureMapping('LHandOnHip_RArm_GestureOffer', '', 'OFFER', 'LHandOnHip_Motex', 'RIGHT_HAND', '')
-		gMap.addGestureMapping('LHandOnHip_RArm_SweepRight', '', 'SWEEP', 'LHandOnHip_Motex', 'RIGHT_HAND', '')
-		gMap.addGestureMapping('LHandOnHip_RArm_SweepRight', '', 'YOU', 'ChrUtah_Idle003', 'RIGHT_HAND', '')
+		# gMap.addGestureMapping(animationName, lexeme, type, hand, style, posture)
+		
+		gMap.addGestureMapping('HandsAtSide_Arms_Sweep', 'SWEEP', '', 'BOTH_HANDS', '', 'HandsAtSide_Motex')
+		gMap.addGestureMapping('HandsAtSide_RArm_GestureYou', 'YOU', '', 'RIGHT_HAND', '', 'HandsAtSide_Motex')
+		gMap.addGestureMapping('LHandOnHip_Arms_GestureWhy', 'WHY', '', 'BOTH_HANDS', '', 'LHandOnHip_Motex')
+		gMap.addGestureMapping('LHandOnHip_RArm_GestureOffer', 'OFFER', '', 'RIGHT_HAND', '', 'LHandOnHip_Motex')
+		gMap.addGestureMapping('LHandOnHip_RArm_SweepRight', 'SWEEP', '', 'RIGHT_HAND', '', 'LHandOnHip_Motex')
+		
+		gMap.addGestureMapping('HandsAtSide_RArm_GestureYou', 'YOU', '', 'RIGHT_HAND', '', 'ChrUtah_Idle003')
+		gMap.addGestureMapping('LHandOnHip_RArm_SweepRight', 'SWEEP', '', 'RIGHT_HAND', '', 'ChrUtah_Idle003')
+		gMap.addGestureMapping('LHandOnHip_Arms_GestureWhy', 'WHY', '', 'BOTH_HANDS', '', 'ChrUtah_Idle003')
+		gMap.addGestureMapping('LHandOnHip_RArm_GestureOffer', 'OFFER', '', 'RIGHT_HAND', '', 'ChrUtah_Idle003')
+		gMap.addGestureMapping('ChrUtah_IndicateThereLeft001', 'LEFT', '', 'LEFT_HAND', '', 'ChrUtah_Idle003')
+		gMap.addGestureMapping('ChrUtah_IndicateThereRight001', 'RIGHT', '', 'RIGHT_HAND', '', 'ChrUtah_Idle003')
 
-def playGesture(chrName, gestureName):
-	bml.execBML(chrName, '<gesture lexeme="' + gestureName + '"/>')
+def playGesture(chrName, gestureName, hand):
+	''' Chararacter name to play gesture, lexeme of gesture, hand to use 
+		Example: playGesture('brad', 'YOU', 'RIGHT_HAND') '''
+	bml.execBML(chrName, '<gesture lexeme="' + gestureName + '" mode="' + hand + '"/>')
 	
