b0c9b62e551b12f4af1bd59f691bb0e19a019d2c
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-12-05 01:49:25 +0000

    More fixes for persistance (saving to a .py file). Fixed problem when saving MotionBlendsBase (blends that use inverse blending, KNN, or RBF). Saving character after converting with a behavior set is more robust now.

--- core/smartbody/smartbody-lib/src/sb/SBMotionBlendBase.h
+++ core/smartbody/smartbody-lib/src/sb/SBMotionBlendBase.h
@@ -15,6 +15,12 @@ namespace SmartBody {
 			SBAPI SBMotionBlendBase();
 			SBAPI SBMotionBlendBase(const std::string& name, const std::string& skelName, int dimension);
 			SBAPI ~SBMotionBlendBase();			
+
+			SBAPI int getNumDimensions();
+			SBAPI const std::string& getSkeleton();
+			SBAPI const std::string& getInterpType();
+			SBAPI const std::string& getBlendType();
+
 			SBAPI virtual void addMotion(const std::string& motion, std::vector<double>& parameter);
 			SBAPI virtual void setMotionParameter(const std::string& motion, std::vector<double>& parameter);
 			SBAPI virtual std::vector<double> getMotionParameter(const std::string& motion);
@@ -31,11 +37,14 @@ namespace SmartBody {
 			SBAPI virtual void getParametersFromWeights(float& x, float& y, float& z, std::vector<double>& weights);
 			SBAPI void addTetrahedron(const std::string& motion1, const std::string& motion2, const std::string& motion3, const std::string& motion4);
 
+			SBAPI virtual std::string saveToString();
+
 		protected:
 			MeCtBlendEngine* blendEngine;			
 			int parameterDim;
 			std::string interpType;		
 			std::string skeletonName;
+			std::string blendType;
 
 	};
 }
