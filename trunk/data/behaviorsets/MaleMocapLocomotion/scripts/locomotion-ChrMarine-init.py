40592a61a45b1d04f773a1ecbda495f19b1cba61
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-12-14 21:53:33 +0000

    Add MocapLocomotion behavior setup scripts. Right now only have locomotionState working. Still need to work on other states like idle-turn, step, etc.

--- trunk/data/behaviorsets/MaleMocapLocomotion/scripts/locomotion-ChrMarine-init.py
+++ trunk/data/behaviorsets/MaleMocapLocomotion/scripts/locomotion-ChrMarine-init.py
@@ -1,42 +1,65 @@
 #locomotion mirror
-mirrorMotion = scene.getMotion("ChrMarine@WalkCircleRt01")
-mirrorMotion.mirror("ChrMarine@WalkCircleLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@WalkTightCircleRt01")
-mirrorMotion.mirror("ChrMarine@WalkTightCircleLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@StrafeFastRt01")
-mirrorMotion.mirror("ChrMarine@StrafeFastLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@RunCircleRt01")
-mirrorMotion.mirror("ChrMarine@RunCircleLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@RunTightCircleRt01")
-mirrorMotion.mirror("ChrMarine@RunTightCircleLf01", "ChrBackovic.sk")
+if scene.getMotion('ChrMarine@WalkCircleLf01') == None:
+	#locomotion smooth cycle
+	smoothMotion = scene.getMotion("ChrMarine@RunCircleRt01")
+	smoothMotion.smoothCycle("ChrMarine@RunCircleRt01_smooth",0.1);
+	smoothMotion = scene.getMotion("ChrMarine@WalkCircleRt01")
+	smoothMotion.smoothCycle("ChrMarine@WalkCircleRt01_smooth",0.1);
+	smoothMotion = scene.getMotion("ChrMarine@WalkTightCircleRt01")
+	smoothMotion.smoothCycle("ChrMarine@WalkTightCircleRt01_smooth",0.1);
+	smoothMotion = scene.getMotion("ChrMarine@StrafeFastRt01")
+	smoothMotion.smoothCycle("ChrMarine@StrafeFastRt01_smooth",0.1);
 
-#idle turn mirror
-mirrorMotion = scene.getMotion("ChrMarine@Turn90Rt")
-mirrorMotion.mirror("ChrMarine@Turn90Lf", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Turn180Rt")
-mirrorMotion.mirror("ChrMarine@Turn180Lf", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Turn360Rt01")
-mirrorMotion.mirror("ChrMarine@Turn360Lf01", "ChrBackovic.sk")
+	#locomotion mirror
+	mirrorMotion = scene.getMotion("ChrMarine@WalkCircleRt01")
+	mirrorMotion.mirror("ChrMarine@WalkCircleLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@WalkTightCircleRt01")
+	mirrorMotion.mirror("ChrMarine@WalkTightCircleLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@StrafeFastRt01")
+	mirrorMotion.mirror("ChrMarine@StrafeFastLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@StrafeSlowRt01")
+	mirrorMotion.mirror("ChrMarine@StrafeSlowLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@RunCircleRt01")
+	mirrorMotion.mirror("ChrMarine@RunCircleLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@RunTightCircleRt01")
+	mirrorMotion.mirror("ChrMarine@RunTightCircleLf01", "ChrBackovic.sk")
+	
+	#idle turn
+	mirrorMotion = scene.getMotion("ChrMarine@Turn90Rt01")
+	mirrorMotion.mirror("ChrMarine@Turn90Lf01", "ChrBackovic.sk")
+	mirrorMotion1 = scene.getMotion("ChrMarine@Turn180Rt01")
+	mirrorMotion1.mirror("ChrMarine@Turn180Lf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@Turn360Rt01")
+	mirrorMotion.mirror("ChrMarine@Turn360Lf01", "ChrBackovic.sk")
 
-#starting mirror
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01")
-mirrorMotion.mirror("ChrMarine@Idle01_ToWalkLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01_Turn90Rt01")
-mirrorMotion.mirror("ChrMarine@Idle01_ToWalk01_Turn90Lf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01_Turn180Rt01")
-mirrorMotion.mirror("ChrMarine@Idle01_ToWalk01_Turn180Lf01", "ChrBackovic.sk")
+	#mirroring for smooth cycle motion 
+	mirrorMotion = scene.getMotion("ChrMarine@WalkCircleRt01_smooth")
+	mirrorMotion.mirror("ChrMarine@WalkCircleLf01_smooth", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@WalkTightCircleRt01_smooth")
+	mirrorMotion.mirror("ChrMarine@WalkTightCircleLf01_smooth", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@StrafeFastRt01_smooth")
+	mirrorMotion.mirror("ChrMarine@StrafeFastLf01_smooth", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@RunCircleRt01_smooth")
+	mirrorMotion.mirror("ChrMarine@RunCircleLf01_smooth", "ChrBackovic.sk")
 
-#step mirror
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepBackwardsRt01")
-mirrorMotion.mirror("ChrMarine@Idle01_StepBackwardsLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepForwardRt01")
-mirrorMotion.mirror("ChrMarine@Idle01_StepForwardLf01", "ChrBackovic.sk")
-mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepSidewaysRt01")
-mirrorMotion.mirror("ChrMarine@Idle01_StepSidewaysLf01", "ChrBackovic.sk")
+	
+	#starting mirror
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01")
+	mirrorMotion.mirror("ChrMarine@Idle01_ToWalkLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01_Turn90Rt01")
+	mirrorMotion.mirror("ChrMarine@Idle01_ToWalk01_Turn90Lf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_ToWalk01_Turn180Rt01")
+	mirrorMotion.mirror("ChrMarine@Idle01_ToWalk01_Turn180Lf01", "ChrBackovic.sk")
 
-scene.run("locomotion-ChrMarine-state-Locomotion.py")
-scene.run("locomotion-ChrMarine-state-IdleTurn.py")
-scene.run("locomotion-ChrMarine-state-StartingLeft.py")
-scene.run("locomotion-ChrMarine-state-StartingRight.py")
-scene.run("locomotion-ChrMarine-state-Step.py")
+	#step mirror
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepBackwardsRt01")
+	mirrorMotion.mirror("ChrMarine@Idle01_StepBackwardsLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepForwardRt01")
+	mirrorMotion.mirror("ChrMarine@Idle01_StepForwardLf01", "ChrBackovic.sk")
+	mirrorMotion = scene.getMotion("ChrMarine@Idle01_StepSidewaysRt01")
+	mirrorMotion.mirror("ChrMarine@Idle01_StepSidewaysLf01", "ChrBackovic.sk")
+
+	
+
+
 
--- trunk/data/sbm-common/scripts/behaviorsets/BehaviorSetMaleMocapLocomotion.py
+++ trunk/data/sbm-common/scripts/behaviorsets/BehaviorSetMaleMocapLocomotion.py
@@ -9,45 +9,75 @@ def setupBehaviorSet():
 	scene.run("zebra2-map.py")
 	zebra2Map = scene.getJointMapManager().getJointMap("zebra2")
 	bradSkeleton = scene.getSkeleton("ChrBackovic.sk")
-	zebra2Map.applySkeleton(bradSkeleton)
+	zebra2Map.applySkeleton(bradSkeleton)	
+	locoMotions = StringVec()
+	locoMotions.append("ChrMarine@Idle01")
+	locoMotions.append("ChrMarine@Jog01")
+	locoMotions.append("ChrMarine@Meander01")
+	locoMotions.append("ChrMarine@Run01")	
+	locoMotions.append("ChrMarine@RunCircleRt01")
+	locoMotions.append("ChrMarine@RunTightCircleRt01")
+	locoMotions.append("ChrMarine@StrafeSlowRt01")
+	locoMotions.append("ChrMarine@Walk01")
+	locoMotions.append("ChrMarine@WalkCircleRt01")
+	locoMotions.append("ChrMarine@WalkTightCircleRt01")
+	locoMotions.append("ChrMarine@Turn360Rt01")
+	locoMotions.append("ChrMarine@StrafeFastRt01")
+	locoMotions.append("ChrMarine@Idle01_StepBackwardsRt01")
+	locoMotions.append("ChrMarine@Idle01_StepForwardRt01")
+	locoMotions.append("ChrMarine@Idle01_StepSidewaysRt01")
+	locoMotions.append("ChrMarine@Turn180Lf01")
+	locoMotions.append("ChrMarine@Turn180Rt01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn90Rt01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn180Rt01")
+	for i in range(0, len(locoMotions)):
+		motion = scene.getMotion(locoMotions[i])
+		zebra2Map.applyMotion(motion)
 
 def retargetBehaviorSet(charName, skelName):
-	locoMotions = StringVec()
-	locoMotions.append("ChrUtah_Walk001")
-	locoMotions.append("ChrUtah_Idle001")
-	locoMotions.append("ChrUtah_Idle01_ToWalk01_Turn360Lf01")
-	locoMotions.append("ChrUtah_Idle01_ToWalk01_Turn360Rt01")
-	locoMotions.append("ChrUtah_Meander01")
-	locoMotions.append("ChrUtah_Shuffle01")
-	locoMotions.append("ChrUtah_Jog001")
-	locoMotions.append("ChrUtah_Run001")
-	locoMotions.append("ChrUtah_WalkInCircleLeft001")
-	locoMotions.append("ChrUtah_WalkInCircleRight001")
-	locoMotions.append("ChrUtah_WalkInTightCircleLeft001")
-	locoMotions.append("ChrUtah_WalkInTightCircleRight001")
-	locoMotions.append("ChrUtah_RunInCircleLeft001")
-	locoMotions.append("ChrUtah_RunInCircleRight001")
-	locoMotions.append("ChrUtah_RunInTightCircleLeft01")
-	locoMotions.append("ChrUtah_RunInTightCircleRight01")
-	locoMotions.append("ChrUtah_StrafeSlowRt01")
-	locoMotions.append("ChrUtah_StrafeSlowLf01")
-	locoMotions.append("ChrUtah_StrafeFastRt01")
-	locoMotions.append("ChrUtah_StrafeFastLf01")
-	locoMotions.append("ChrUtah_Idle001")
-	locoMotions.append("ChrUtah_Turn90Lf01")
-	locoMotions.append("ChrUtah_Turn180Lf01")
-	locoMotions.append("ChrUtah_Turn90Rt01")
-	locoMotions.append("ChrUtah_Turn180Rt01")
-	locoMotions.append("ChrUtah_StopToWalkRt01")
-	locoMotions.append("ChrUtah_Idle01_ToWalk01_Turn90Lf01")
-	locoMotions.append("ChrUtah_Idle01_ToWalk01_Turn180Lf01")
-	locoMotions.append("ChrUtah_Idle01_StepBackwardRt01")
-	locoMotions.append("ChrUtah_Idle01_StepForwardRt01")
-	locoMotions.append("ChrUtah_Idle01_StepSidewaysRt01")
-	locoMotions.append("ChrUtah_Idle01_StepBackwardLf01")
-	locoMotions.append("ChrUtah_Idle01_StepForwardLf01")
-	locoMotions.append("ChrUtah_Idle01_StepSidewaysLf01")
+
+	scene.run('locomotion-ChrMarine-init.py')
 	
+	locoMotions = StringVec()
+	locoMotions.append("ChrMarine@Idle01")
+	locoMotions.append("ChrMarine@Jog01")
+	locoMotions.append("ChrMarine@Meander01")
+	locoMotions.append("ChrMarine@Run01")
+	locoMotions.append("ChrMarine@RunCircleLf01_smooth")
+	locoMotions.append("ChrMarine@RunCircleRt01_smooth")
+	locoMotions.append("ChrMarine@RunTightCircleLf01")
+	locoMotions.append("ChrMarine@RunTightCircleRt01")
+	locoMotions.append("ChrMarine@StrafeSlowLf01")
+	locoMotions.append("ChrMarine@StrafeSlowRt01")
+	locoMotions.append("ChrMarine@Walk01")
+	locoMotions.append("ChrMarine@WalkCircleLf01_smooth")
+	locoMotions.append("ChrMarine@WalkCircleRt01_smooth")
+	locoMotions.append("ChrMarine@WalkTightCircleLf01_smooth")
+	locoMotions.append("ChrMarine@WalkTightCircleRt01_smooth")
+	locoMotions.append("ChrMarine@Turn360Lf01")
+	locoMotions.append("ChrMarine@Turn360Rt01")
+	locoMotions.append("ChrMarine@StrafeFastLf01_smooth")
+	locoMotions.append("ChrMarine@StrafeFastRt01_smooth")
+	locoMotions.append("ChrMarine@Idle01_StepBackwardsLf01")
+	locoMotions.append("ChrMarine@Idle01_StepBackwardsRt01")
+	locoMotions.append("ChrMarine@Idle01_StepForwardLf01")
+	locoMotions.append("ChrMarine@Idle01_StepForwardRt01")
+	locoMotions.append("ChrMarine@Idle01_StepSidewaysLf01")
+	locoMotions.append("ChrMarine@Idle01_StepSidewaysRt01")
+	locoMotions.append("ChrMarine@Turn90Lf01")
+	locoMotions.append("ChrMarine@Turn180Lf01")
+	locoMotions.append("ChrMarine@Turn90Rt01")
+	locoMotions.append("ChrMarine@Turn180Rt01")
+	locoMotions.append("ChrMarine@Idle01_ToWalkLf01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn90Lf01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn180Lf01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn90Rt01")
+	locoMotions.append("ChrMarine@Idle01_ToWalk01_Turn180Rt01")
+	
+	
+		
 	outDir = '../../../../data/retarget/motion/' + skelName + '/';
 	if not os.path.exists(outDir):
 		os.makedirs(outDir)
@@ -56,28 +86,25 @@ def retargetBehaviorSet(charName, skelName):
 	for n in range(0, len(locoMotions)):
 		curMotion = scene.getMotion(locoMotions[n])
 		if curMotion is not None:
-			retargetMotion(locoMotions[n], 'test_utah.sk', skelName, outDir + 'MaleMocapLocomotion/');
+			retargetMotion(locoMotions[n], 'ChrBackovic.sk', skelName, outDir + 'MaleMocapLocomotion/');
 
 	# setup standard locomotion
-	scene.run("stateAllLocomotion.py")
-	locomotionSetup(skelName, "base", skelName, skelName)
-	
+	scene.run("locomotion-ChrMarine-state-Locomotion.py")
+	marineLocomotionSetup(skelName, "base", skelName, skelName)
+	'''
 	# starting state, starting locomotion with different angle
-	scene.run("stateAllStarting.py")
+	scene.run("locomotion-ChrMarine-state-StartingLeft.py")
 	startingSetup(skelName, "base", skelName, skelName)
 
 	# idle turn state, facing adjusting
-	scene.run("stateAllIdleTurn.py")
+	scene.run("locomotion-ChrMarine-state-IdleTurn.py")
 	idleTurnSetup(skelName, "base", skelName, skelName)
 
 	# step state, stepping adjusting
-	scene.run("stateAllStep.py")
+	scene.run("locomotion-ChrMarine-state-Step.py")
 	stepSetup(skelName, "base", skelName, skelName)
-
-	# transitions
-	scene.run("transitions.py")
-	transitionSetup(skelName, skelName)
-	
+	'''
+		
 	# setup steering
 	scene.run("init-steer-agents.py")
 	steerManager = scene.getSteerManager()
