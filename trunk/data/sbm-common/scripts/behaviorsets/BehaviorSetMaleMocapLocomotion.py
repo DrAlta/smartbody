403ce2ad96528b6670c75b693b26610351140e43
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-12-15 01:31:21 +0000

    More fix to mocap locomotion setup. Now all the states are ready and the steering works fine on retarget character ( tested with ChrBrad ).

--- trunk/data/sbm-common/scripts/behaviorsets/BehaviorSetMaleMocapLocomotion.py
+++ trunk/data/sbm-common/scripts/behaviorsets/BehaviorSetMaleMocapLocomotion.py
@@ -15,21 +15,21 @@ def setupBehaviorSet():
 	locoMotions.append("ChrMarine@Jog01")
 	locoMotions.append("ChrMarine@Meander01")
 	locoMotions.append("ChrMarine@Run01")
-	locoMotions.append("ChrMarine@RunCircleLf01_smooth")
-	locoMotions.append("ChrMarine@RunCircleRt01_smooth")
+	locoMotions.append("ChrMarine@RunCircleLf01")
+	locoMotions.append("ChrMarine@RunCircleRt01")
 	locoMotions.append("ChrMarine@RunTightCircleLf01")
 	locoMotions.append("ChrMarine@RunTightCircleRt01")
 	locoMotions.append("ChrMarine@StrafeSlowLf01")
 	locoMotions.append("ChrMarine@StrafeSlowRt01")
 	locoMotions.append("ChrMarine@Walk01")
-	locoMotions.append("ChrMarine@WalkCircleLf01_smooth")
-	locoMotions.append("ChrMarine@WalkCircleRt01_smooth")
-	locoMotions.append("ChrMarine@WalkTightCircleLf01_smooth")
-	locoMotions.append("ChrMarine@WalkTightCircleRt01_smooth")
+	locoMotions.append("ChrMarine@WalkCircleLf01")
+	locoMotions.append("ChrMarine@WalkCircleRt01")
+	locoMotions.append("ChrMarine@WalkTightCircleLf01")
+	locoMotions.append("ChrMarine@WalkTightCircleRt01")
 	locoMotions.append("ChrMarine@Turn360Lf01")
 	locoMotions.append("ChrMarine@Turn360Rt01")
-	locoMotions.append("ChrMarine@StrafeFastLf01_smooth")
-	locoMotions.append("ChrMarine@StrafeFastRt01_smooth")
+	locoMotions.append("ChrMarine@StrafeFastLf01")
+	locoMotions.append("ChrMarine@StrafeFastRt01")
 	locoMotions.append("ChrMarine@Idle01_StepBackwardsLf01")
 	locoMotions.append("ChrMarine@Idle01_StepBackwardsRt01")
 	locoMotions.append("ChrMarine@Idle01_StepForwardLf01")
@@ -37,6 +37,73 @@ def setupBehaviorSet():
 	locoMotions.append("ChrMarine@Idle01_StepSidewaysLf01")
 	locoMotions.append("ChrMarine@Idle01_StepSidewaysRt01")
 	locoMotions.append("ChrMarine@Turn90Lf01")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	locoMotions.append("ChrMarine@Turn180Lf01")
 	locoMotions.append("ChrMarine@Turn90Rt01")
 	locoMotions.append("ChrMarine@Turn180Rt01")
@@ -108,8 +175,12 @@ def retargetBehaviorSet(charName, skelName):
 	marineLocomotionSetup(skelName, "base", skelName, skelName)
 	
 	# starting state, starting locomotion with different angle
-	#scene.run("locomotion-ChrMarine-state-StartingLeft.py")
-	#startingSetup(skelName, "base", skelName, skelName)
+	scene.run("locomotion-ChrMarine-state-StartingLeft.py")
+	mocapStartLeftSetup(skelName, "base", skelName, skelName)
+	
+	# starting state, starting locomotion with different angle
+	scene.run("locomotion-ChrMarine-state-StartingRight.py")
+	mocapStartRightSetup(skelName, "base", skelName, skelName)
 
 	# idle turn state, facing adjusting
 	scene.run("locomotion-ChrMarine-state-IdleTurn.py")
