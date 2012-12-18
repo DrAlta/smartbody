6d2e56b2e891ea0d48a3d223ed9ca2aa641607b1
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2012-12-18 21:39:57 +0000

    Update JointMapViewer. Now the joint names can be selected from a drop down list, and the filter is applied to list only matching joint names from the skeleton.

--- core/smartbody/sbm/src/jointmapviewer/JointMapViewer.h
+++ core/smartbody/sbm/src/jointmapviewer/JointMapViewer.h
@@ -4,6 +4,7 @@
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Choice.H>
 #include <FL/Fl_Scroll.H>
+#include <FL/Fl_Input_Choice.H>
 #include <string>
 
 class JointMapViewer : public Fl_Double_Window
@@ -15,20 +16,24 @@ class JointMapViewer : public Fl_Double_Window
 		static void ApplyMapCB(Fl_Widget* widget, void* data);
 		static void CancelCB(Fl_Widget* widget, void* data);
 		static void SelectMapCB(Fl_Widget* widget, void* data);
+		static void SelectCharacterCB(Fl_Widget* widget, void* data);
+		static void JointNameChange(Fl_Widget* widget, void* data);
 
 		void applyJointMap();
 		void updateSelectMap();
+		void updateCharacter();
 		void updateUI();
-		
+		void updateJointName(Fl_Input_Choice* jointChoice);				
 
 	protected:
 		std::string _jointMapName;
 		std::string _skelName;
 		std::vector<std::string> standardJointNames;
+		std::vector<std::string> skeletonJointNames;
 
 		Fl_Choice* _choiceJointMaps;
 		Fl_Choice* _choiceCharacters;
 		Fl_Scroll* _scrollGroup;
-
+		std::vector<Fl_Input_Choice*> _jointChoiceList;
 };
 #endif
