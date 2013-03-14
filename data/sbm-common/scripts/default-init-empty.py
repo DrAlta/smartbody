dc45eacda2b5d599da955fc197b7573d03e751dc
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-03-14 08:56:01 +0000

    Added characterSetup() function to default-init-empty.py which allows BMLR characters to use locomotion, steering and full body IK automatically.

--- data/sbm-common/scripts/default-init-empty.py
+++ data/sbm-common/scripts/default-init-empty.py
@@ -2,6 +2,17 @@ print "|--------------------------------------------|"
 print "|  data/sbm-common/scripts/default-init.py  |"
 print "|--------------------------------------------|"
 
+def characterSetup(charName):
+	# steering
+	steerManager = scene.getSteerManager()
+	setupSteerAgent(charName, 'all')	
+	steerManager.setEnable(True)
+	
+	# reaching
+	reachSetup(charName,"KNN","")
+
+
+
 scene.run("default-viewer.py")
 
 ### Load data/sbm-common assets
@@ -9,7 +20,13 @@ scene.addAssetPath("seq", "sbm-common/scripts")
 
 scene.run("init-common-assets.py")
 scene.run("init-common-face.py")
-scene.run("init-utah-face.py")
+
+scene.run("init-param-animation.py")
+
+scene.run("init-steer-agents.py")
+scene.run("init-example-reach.py")
+
+scene.setBoolAttribute("internalAudio", True)
 
 # start the simulation
 sim.start()
