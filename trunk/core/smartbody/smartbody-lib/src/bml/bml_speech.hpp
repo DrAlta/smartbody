6d449aa96a1fa8f52a70e06fb6377ac2da87d3fb
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-04-04 01:09:21 +0000

    - Fix and improve curve stitching and smoothing.

--- trunk/core/smartbody/smartbody-lib/src/bml/bml_speech.hpp
+++ trunk/core/smartbody/smartbody-lib/src/bml/bml_speech.hpp
@@ -181,7 +181,9 @@ namespace BML {
 
 		std::vector<float> stitchCurve(std::vector<float>& c1, std::vector<float>& c2);
 
-		void smoothCurve(std::vector<float>& c, std::vector<float>& timeMarkers, float windowSize);
+		void smoothCurve(std::vector<float>& c, std::vector<float>& timeMarkers, float windowSize, float speedLimit);
+
+		bool getLineIntersection(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, float p3_y, float& i_x, float& i_y);
 
 		friend class BML::Processor;  //temporary
 	};
