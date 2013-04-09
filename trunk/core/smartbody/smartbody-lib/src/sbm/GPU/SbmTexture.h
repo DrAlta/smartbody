59303e24d5af5387323712f9e112d2097c54b715
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-04-09 15:17:00 +0000

    GL -> OpenGL for OSx build

--- trunk/core/smartbody/smartbody-lib/src/sbm/GPU/SbmTexture.h
+++ trunk/core/smartbody/smartbody-lib/src/sbm/GPU/SbmTexture.h
@@ -9,8 +9,13 @@
 #ifdef WIN32
 #include <windows.h>
 #endif
+#ifdef __APPLE__
+# include <OpenGL/gl.h>
+# include <OpenGL/glu.h>
+#else
 # include <GL/gl.h>
 # include <GL/glu.h>
+#endif
 #endif
 #include <map>
 #include <string>
