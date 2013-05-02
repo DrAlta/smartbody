6f0356d1f8a6726f67647ccf1ea30e9beb68d271
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-05-02 00:37:59 +0000

    Fixed PandaBMLR scripts to use proper Panda libraries.

--- trunk/core/PandaBMLR/RUN_DemoBasicEmbedded.bat
+++ trunk/core/PandaBMLR/RUN_DemoBasicEmbedded.bat
@@ -1,2 +1,1 @@
-rem ..\..\lib\Panda3D\python\ppython.exe -E DemoBasic.py
-..\..\lib\Panda3D-1.7.2\python\ppython.exe -E DemoBasicEmbedded.py
+..\..\lib\Panda3D\python\ppython.exe -E DemoBasicEmbedded.py
