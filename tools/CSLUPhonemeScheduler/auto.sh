6395d7b81ddb3acf28457ae5c066d78ec98b3e41
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-05-17 22:40:19 +0000

    - Modified auto.sh

--- tools/CSLUPhonemeScheduler/auto.sh
+++ tools/CSLUPhonemeScheduler/auto.sh
@@ -3,8 +3,9 @@ ls ../files/*.txt | while read line; do
 	filename=$(basename "$line")
 	echo $filename
 	filename="${filename%.*}"
-	../Tcl80/bin/tclsh80.exe gen_pronun.tcl ../files/$filename.txt ${filename}_automatic.ntp
-	../Tcl80/bin/tclsh80.exe fa_new_multichan.tcl ../files/$filename.wav ${filename}_automatic.ntp ../files/$filename.txt1
+	../Tcl80/bin/tclsh80.exe gen_pronun.tcl ../files/$filename.txt ${filename}.ntp
+	../Tcl80/bin/tclsh80.exe fa_new_multichan.tcl ../files/$filename.wav ${filename}.ntp ../files/$filename.txt1
+	mv ${filename}.ntp ../files
 done
 cd ..
 cd converter
