8b0186e79c1c53dbe1fbdf3f956602ce714d2684
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-06-11 00:57:33 +0000

    Add initial sets of tutorial scripts to accommodate the tutorials section in SmartBody documentations. Currently it covers calling SmartBody Python API, creating a simple pawn, loading various assets, and creating a simple character. More to be done about using BML to create character behaviors, using behavior sets, speech, locomotion, etc.

---
+++ data/examples/Tutorials/4_CreatingCharacter.py
@@ -0,0 +1,33 @@
+print "|--------------------------------------------|"
+print "|         Starting Tutorial 4	            |"
+print "|--------------------------------------------|"
+
+print 'media path = ' + scene.getMediaPath()
+# Add asset paths
+assetManager = scene.getAssetManager()
+assetManager.addAssetPath('motion', 'ChrBrad')
+assetManager.addAssetPath('mesh', 'mesh')
+assetManager.addAssetPath('script', 'scripts')
+# Load assets based on asset paths
+assetManager.loadAssets()
+
+# set the camera
+scene.setScale(1.0)
+scene.run('default-viewer.py')
+
+# run a Python script file
+scene.run('zebra2-map.py')
+zebra2Map = scene.getJointMapManager().getJointMap('zebra2')
+bradSkeleton = scene.getSkeleton('ChrBrad.sk')
+zebra2Map.applySkeleton(bradSkeleton)
+zebra2Map.applyMotionRecurse('ChrBrad')
+
+# Set up Brad
+brad = scene.createCharacter('ChrBrad', '')
+bradSkeleton = scene.createSkeleton('ChrBrad.sk')
+brad.setSkeleton(bradSkeleton)
+# Set standard controller
+brad.createStandardControllers()
+# Deformable mesh
+brad.setDoubleAttribute('deformableMeshScale', .01)
+brad.setStringAttribute('deformableMesh', 'ChrBrad')
