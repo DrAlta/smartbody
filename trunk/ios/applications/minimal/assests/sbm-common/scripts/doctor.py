0a0dd37f2bb9236ce11041eed140cb23a820243b
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-06-20 00:26:30 +0000

    More files added for last commits

--- trunk/ios/applications/minimal/assests/sbm-common/scripts/doctor.py
+++ trunk/ios/applications/minimal/assests/sbm-common/scripts/doctor.py
@@ -1,43 +1,34 @@
 print "|--------------------------------------------|"
-print "|  data/sbm-common/scripts/doctor.py  |"
+print "|  data/sbm-common/scripts/default-init.py   |"
 print "|--------------------------------------------|"
 
 
 ### Load data/sbm-common assets
 scene.addAssetPath("script", "sbm-common/scripts")
-scene.addAssetPath("mesh", "mesh")
-scene.addAssetPath("audio", ".")
 scene.addAssetPath("motion", "sbm-common/common-sk")
+scene.addAssetPath("mesh", "./mesh")
 scene.loadAssets()
 
 scene.setBoolAttribute("internalAudio", True)
 
-camera = scene.getActiveCamera()
-camera.setEye(0, 20, 200)
-camera.setCenter(0, 0, 0)
-camera.setUpVector(SrVec(0, 1, 0))
-camera.setScale(1)
-camera.setFov(1)
-camera.setFarPlane(1000)
-camera.setNearPlane(0.1)
-camera.setAspectRatio(1)
-
-
 scene.run("init-common-face.py")
 
-doctor = scene.createCharacter("doctor", "")
-doctorSkeleton = scene.createSkeleton("common.sk")
-doctor.setSkeleton(doctorSkeleton)
-doctor.setFaceDefinition(defaultFace)
-doctorPos = SrVec(0, 0, 0)
-doctor.setPosition(doctorPos)
-doctorHPR = SrVec(0, 0, 0)
-doctor.setHPR(doctorHPR)
-doctor.setVoice("audiofile")
-doctor.setVoiceCode("Sounds")
-doctor.createStandardControllers()
-doctor.setStringAttribute("deformableMesh", "doctor")
+# camera
+camera = scene.getActiveCamera()
+camera.setCenter(0, 92, 0);
+camera.setUpVector(SrVec(0, 1, 0))
+camera.setEye( 0, 166, 300 )
 
+brad = scene.createCharacter("doctor", "")
+bradSkeleton = scene.createSkeleton("common.sk")
+brad.setSkeleton(bradSkeleton)
+brad.setFaceDefinition(defaultFace)
+bradPos = SrVec(0, 107, 0)
+brad.setPosition(bradPos)
+bradHPR = SrVec(0, 0, 0)
+brad.setHPR(bradHPR)
+brad.createStandardControllers()
+brad.setStringAttribute("deformableMesh", "doctor")
 
 # start the simulation
 sim.start()
@@ -45,6 +36,3 @@ bml.execBML('doctor', '<body posture="LHandOnHip_Motex"/>')
 bml.execBML('doctor', '<saccade mode="listen"/>')
 sim.resume()
 
-# start the flash gaze controller
-bml.execBML('doctor', '<gaze sbm:handle="flash" sbm:target-pos="0 0 0"/>')
-bml.execBML('doctor', '<gaze sbm:handle="flash" sbm:fade-out="0.2"/>')
