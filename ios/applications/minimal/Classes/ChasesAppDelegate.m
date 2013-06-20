f289853088bdf4a4a2b6161bab97da64cbba910a
yuyuxu <yuyuxu@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-06-20 00:24:17 +0000

    Re-write SmartBody iOS minimal project so it renders character mesh properly and use the up-to-date API

--- ios/applications/minimal/Classes/ChasesAppDelegate.m
+++ ios/applications/minimal/Classes/ChasesAppDelegate.m
@@ -35,28 +35,4 @@
 	[m_rootViewController release];
 	[super dealloc];
 }
-
-/*
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
-	glView.animationInterval = 2.0 / 60.0;
-	[glView startAnimation];
-}
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-	glView.animationInterval = 1.0 / 5.0;
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-	glView.animationInterval = 2.0 / 60.0;
-}
-
-
-- (void)dealloc {
-	[window release];
-	[glView release];
-	[super dealloc];
-}
-*/
 @end
