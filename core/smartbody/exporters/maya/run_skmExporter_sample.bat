15e67b2f38f436c060c7e66df0b9c7d74e76a2f2
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-07-30 19:18:57 +0000

    Removed core/smartbody/exporters/maya directory - not being used anymore.

--- core/smartbody/exporters/maya/run_skmExporter_sample.bat
+++
@@ -1,5 +0,0 @@
-
-@rem usage:  call skmExporter.bat "<source maya file>" "<dest dir>/"
-
-call skmExporter.bat "C:/art/SBM_MayaProject/scenes/sbm_Proxy2.ma" "./New Folder/"
-call skmExporter.bat "C:/art/SBM_MayaProject/scenes/sbm_Proxy2.ma" ""
