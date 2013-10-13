6b8ad1f09bbfd36178e75172cba41f3e65e5c101
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-10-13 06:15:15 +0000

    Added some more asset handlers with empty implementations.

---
+++ core/smartbody/SmartBody/src/sb/SBAssetHandlerAmc.cpp
@@ -0,0 +1,70 @@
+#include "SBAssetHandlerAmc.h"
+#include <vhcl.h>
+#include <boost/version.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/convenience.hpp>
+#include <boost/algorithm/string.hpp>
+#include <sb/SBMotion.h>
+#include <sb/SBScene.h>
+
+namespace SmartBody {
+
+SBAssetHandlerAmc::SBAssetHandlerAmc()
+{
+	assetTypes.push_back("amc");
+}
+
+SBAssetHandlerAmc::~SBAssetHandlerAmc()
+{
+}
+
+std::vector<SBAsset*> SBAssetHandlerAmc::getAssets(const std::string& path)
+{
+	std::vector<SBAsset*> assets;
+
+	boost::filesystem::path pathname(path);
+	if( !boost::filesystem::exists( pathname ) )
+	{
+#if (BOOST_VERSION > 104400)
+		LOG("Asset path \"%s\" not found.",  pathname.string().c_str());
+#else
+		LOG("Asset path \"%s\" not found.", pathname.native_file_string().c_str());
+#endif
+		return assets;
+	}
+
+	if( boost::filesystem::is_directory( pathname ) ) // path indicates a directory
+	{
+		#if (BOOST_VERSION > 104400)
+		LOG("Asset path \"%s\" is a directory.",  pathname.string().c_str());
+#else
+		LOG("Asset path \"%s\" is a directory.", pathname.native_file_string().c_str());
+#endif
+		return assets;
+	}
+
+	SmartBody::SBMotion* motion = new SmartBody::SBMotion();
+	bool parseSuccessful = false;
+
+	std::string convertedPath = pathname.string();
+#ifdef WIN32
+	boost::replace_all(convertedPath, "\\", "/");
+#endif
+	SrInput in( convertedPath.c_str(), "rt" );
+	SrString fullin_string;
+	in.getall( fullin_string );
+	SrInput fullin( (const char *)fullin_string );
+	fullin.filename( convertedPath.c_str() ); // copy filename for error message
+			
+	double scale = SmartBody::SBScene::getScene()->getDoubleAttribute("globalMotionScale");
+	parseSuccessful = motion->load( fullin, scale );
+	if (parseSuccessful)
+		assets.push_back(motion);
+	else
+		LOG("Could not .skm file %s", convertedPath.c_str());
+
+	return assets;
+}
+
+};
