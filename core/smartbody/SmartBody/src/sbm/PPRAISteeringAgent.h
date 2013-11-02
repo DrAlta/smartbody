5ee898c14413661a67510f58e68e43daf7ce71f1
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-11-02 00:39:49 +0000

    Add more exception handling for steer agent. Need more test to see if this improves the random crash.
    Initial attempt to add a turning angle epsilon in steering agent to prevent small and high frequency turning. It doesn't seem to remove all the jittering. Need more testing to verify if the same epsilon threshold need to be considered for speed and scoot as well. ( Though it seems there is already a speed and scoot threshold handling this ).

--- core/smartbody/SmartBody/src/sbm/PPRAISteeringAgent.h
+++ core/smartbody/SmartBody/src/sbm/PPRAISteeringAgent.h
@@ -128,6 +128,7 @@ class PPRAISteeringAgent : public SmartBody::SBSteerAgent
 		float acceleration;
 		float scootAcceleration;
 		float angleAcceleration;
+		float turningEpsilon;		
 		SrVec targetLoc;
 		bool stepAdjust;
 		float pedMaxTurningRateMultiplier;
