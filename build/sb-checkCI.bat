16d9d5a0285f272bc41bd792298b8420dff79326
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-11-13 03:50:45 +0000

    build - pass in the email address to the script rather than hard-code it

--- build/sb-checkCI.bat
+++ build/sb-checkCI.bat
@@ -9,6 +9,8 @@ if %ERRORLEVEL% EQU 0 set PY_EXE=mayapy.exe
 if not defined PY_EXE set PY_EXE="%ProgramW6432%\Autodesk\Maya2012\bin\mayapy.exe"
 if defined MAYAINSTALLPATH2012_64 set PY_EXE="%MAYAINSTALLPATH2012_64%\bin\mayapy.exe"
 
-%PY_EXE% sb-automated-build.py checktime svn://svn.code.sf.net/p/smartbody/code/trunk user password 200 d: d:/SBM-Builds -ci 48
+%PY_EXE% sb-automated-build-prepare.py svn://svn.code.sf.net/p/smartbody/code/trunk
+
+%PY_EXE% scripts/sb-automated-build.py checktime svn://svn.code.sf.net/p/smartbody/code/trunk user password 200 d: d:/SBM-Builds -ci 48 nospam@ict.usc.edu
 
 @endlocal
