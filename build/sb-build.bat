71feda4092c26d30df51860bdca7be04a4ea5aa1
fastict <fastict@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2013-11-15 02:31:08 +0000

    build - fixed typo in svn url

--- build/sb-build.bat
+++ build/sb-build.bat
@@ -9,7 +9,7 @@ if %ERRORLEVEL% EQU 0 set PY_EXE=mayapy.exe
 if not defined PY_EXE set PY_EXE="%ProgramW6432%\Autodesk\Maya2012\bin\mayapy.exe"
 if defined MAYAINSTALLPATH2012_64 set PY_EXE="%MAYAINSTALLPATH2012_64%\bin\mayapy.exe"
 
-%PY_EXE% sb-automated-build-prepare.py svn://svn.code.sf.net/p/smartbody/code/trunk
+%PY_EXE% sb-automated-build-prepare.py svn://svn.code.sf.net/p/smartbody/code/trunk/build
 
 %PY_EXE% scripts/sb-automated-build.py build password -ci nospam@ict.usc.edu
 
