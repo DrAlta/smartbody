006f7e6454fd5e20739396abda967cbb938b2ee8
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-03-21 21:51:45 +0000

    Add a quick hack in Python binding to set a deformable model on a pawn without opening FltkViewer/FltkListener.
    Example :
    setPawnMesh('testAutoRig', 'test.ply') # instead of pawn.setStringAttribute("mesh","test.ply")

--- trunk/core/smartbody/sbgui/src/pythonbind/SBPythonAutorig.cpp
+++ trunk/core/smartbody/sbgui/src/pythonbind/SBPythonAutorig.cpp
@@ -1,6 +1,10 @@
 #include "vhcl.h"
 #include "SBPythonAutoRig.h"
 #include <sb/SBObject.h>
+#include <sb/SBScene.h>
+#include <sb/SBAssetManager.h>
+#include <sb/SBPawn.h>
+#include <sbm/GPU/SbmDeformableMeshGPU.h>
 #include <sbm/ParserOpenCOLLADA.h>
 #include <autorig/SBAutoRigManager.h>
 #include <boost/filesystem/operations.hpp>
@@ -18,6 +22,30 @@
 
 #ifndef SB_NO_PYTHON
 
+void setPawnMesh(const std::string& pawnName, const std::string& meshName)
+{
+	SmartBody::SBScene* scene = SmartBody::SBScene::getScene();
+	SmartBody::SBAssetManager* assetManager = scene->getAssetManager();
+	SmartBody::SBPawn* pawn = scene->getPawn(pawnName);
+	if (!pawn)
+		return;
+
+	DeformableMesh* mesh = assetManager->getDeformableMesh(meshName);
+	if (!mesh)
+	{
+		return;
+	}
+	if (mesh)
+	{
+		pawn->dStaticMeshInstance_p = new SbmDeformableMeshGPUInstance();
+		pawn->dStaticMeshInstance_p->setToStaticMesh(true);
+		DeformableMeshInstance* meshInsance = pawn->dStaticMeshInstance_p;
+		meshInsance->setDeformableMesh(mesh);
+		//meshInsance->setSkeleton(pawn->getSkeleton());	
+		meshInsance->setPawn(pawn);
+	}
+}
+
 void saveDeformableMesh(const std::string& meshName, const std::string& skelName, const std::string& outDir)
 {
 	std::vector<std::string> moNames;
@@ -27,6 +55,8 @@ void saveDeformableMesh(const std::string& meshName, const std::string& skelName
 BOOST_PYTHON_MODULE(AutoRig)
 {	
 	boost::python::def("saveDeformableMesh", saveDeformableMesh, "Save the deformable model to the target directory");
+	boost::python::def("setPawnMesh", setPawnMesh, "Set the deformable model to the target pawn");
+
 
 	boost::python::class_<SBAutoRigManager>("SBAutoRigManager")
 		.def("getAutoRigManager", &SBAutoRigManager::singletonPtr, boost::python::return_value_policy<boost::python::reference_existing_object>(), "Get the autorigging manager")
