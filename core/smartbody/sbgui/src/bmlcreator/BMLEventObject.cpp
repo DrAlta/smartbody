c88eea05399e8387d7e2c6d6dbb34fc166ed578a
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-05-15 22:12:00 +0000

    Added both start and stroke GUI bml attributes for <event> message (start = python command, stroke = old style sbm command)

--- core/smartbody/sbgui/src/bmlcreator/BMLEventObject.cpp
+++ core/smartbody/sbgui/src/bmlcreator/BMLEventObject.cpp
@@ -6,7 +6,8 @@ BMLEventObject::BMLEventObject() : BMLObject()
 	setName("sbm:event");
 	createStringAttribute("message", "", true, "Basic", 50, false, false, false, "Command to be sent when event is triggered.");
 
-	createStringAttribute("stroke", "", true, "Basic", 100, false, false, false, "When this event will be triggered.");
+	createStringAttribute("start", "", true, "Basic", 100, false, false, false, "When this event will be triggered. Message will be interpreted as a Python command.");
+	createStringAttribute("stroke", "", true, "Basic", 100, false, false, false, "When this event will be triggered. Message will be interpreted as an old-style sbm command.");
 }
 
 BMLEventObject::~BMLEventObject()
