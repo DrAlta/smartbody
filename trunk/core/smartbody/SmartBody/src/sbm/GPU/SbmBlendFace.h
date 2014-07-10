1ddc51e4d9b24ea63a2cdbb4f7614b7186bcdad8
dancasas <dancasas@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-07-10 20:45:35 +0000

    Added new files required for the previous commit. Sorry I forgot before!

---
+++ trunk/core/smartbody/SmartBody/src/sbm/GPU/SbmBlendFace.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <sb/SBTypes.h>
+#include "SbmShader.h"
+#include "VBOData.h"
+#include "TBOData.h"
+#include <sbm/sbm_deformable_mesh.h>
+
+#if !defined(__FLASHPLAYER__)
+#include "external/glew/glew.h"
+#endif
+
+class SbmDeformableMeshGPU;
+
+class SbmBlendFace: public DeformableMesh
+{
+	public:
+		SbmBlendFace();
+		~SbmBlendFace();
+
+		bool			buildVertexBufferGPU();
+		void			addFace(SbmDeformableMeshGPU*);
+		void			initShaderProgram();
+		void			initShaderProgram_Dan();
+		void			initShader();
+		
+		void			setDeformableMesh(DeformableMesh*);
+		DeformableMesh* getDeformableMesh();
+		
+		VBOVec3f*		getVBOPos(int);
+		VBOVec3f*		getVBONormal();
+		VBOVec2f*		getVBOTexCoord();
+		VBOVec3i*		getVBOTri();
+		
+		std::vector<VBOVec3i*> subMeshTris;
+
+		GLuint			_vsID;
+		GLuint			_fsID;
+		GLuint			_programID;
+
+	private:
+		DeformableMesh*			_mesh;
+		std::vector<VBOVec3f*>	_VBOPos;	
+		VBOVec3f*				_VBONormal;
+		VBOVec2f*				_VBOTexCoord;
+		VBOVec3i*				_VBOTri;
+
+		bool			_initGPUVertexBuffer;
+		
+		std::string		_shaderName;
+
+		unsigned int	_faceCounter;				// How many faces have been loaded
+	
+
+};
+
+
+class SbmBlendTextures
+{
+	public:
+		SbmBlendTextures();
+		~SbmBlendTextures();
+
+		static GLuint getShader();
+		static void BlendTwoFBO(GLuint, GLuint, GLuint, GLuint, float, GLuint, int, int);
+
+		GLuint			_vsID;
+		GLuint			_fsID;
+		GLuint			_programID;
+
+	private:
+		std::string		_shaderName;
+};
