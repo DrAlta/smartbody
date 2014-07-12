7f052efaaff832042481de58b2657e5bf629170e
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-07-12 01:58:13 +0000

    Fixes for osx build

--- trunk/core/smartbody/sbgui/external/polyvox/library/PolyVoxCore/include/PolyVoxCore/SurfaceMesh.h
+++ trunk/core/smartbody/sbgui/external/polyvox/library/PolyVoxCore/include/PolyVoxCore/SurfaceMesh.h
@@ -32,9 +32,15 @@ freely, subject to the following restrictions:
 #include <algorithm>
 #include <cstdlib>
 #include <list>
+#ifdef __APPLE__
+#include <tr1/memory>
+#else
 #include <memory>
+#endif
 #include <set>
 #include <vector>
+
+
 
 namespace PolyVox
 {
