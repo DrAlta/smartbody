9187e85100fe2ead84732fad7a939457395e29bb
madil90 <madil90@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-08-25 21:24:44 +0000

    missing files for hand configuration

---
+++ core/smartbody/SmartBody/src/sb/SBHandConfiguration.cpp
@@ -0,0 +1,80 @@
+#include "SBHandConfiguration.h"
+
+#include <sb/SBMotion.h>
+#include <sb/SBAssetManager.h>
+
+
+namespace SmartBody {
+
+SBHandConfiguration::SBHandConfiguration()
+{
+}
+
+SBHandConfiguration::~SBHandConfiguration()
+{
+	// don't delete any motions here
+	_motions.clear();
+}
+
+void SBHandConfiguration::addMotion(std::string motionName)
+{
+	// extract motion from scene
+	SBMotion* motion = SmartBody::SBScene::getScene()->getAssetManager()->getMotion(motionName);
+
+	if (!motion)
+	{
+		LOG("Unable to find motion named %s",motionName);
+		return;
+	}
+
+	// add motion
+	_motions.push_back(motion);
+}
+
+void SBHandConfiguration::removeMotion(std::string motionName)
+{
+	SBMotion* motion = SmartBody::SBScene::getScene()->getAssetManager()->getMotion(motionName);
+
+	if (!motion)
+	{
+		LOG("Unable to find motion named %s",motionName);
+		return;
+	}
+
+	std::vector<SBMotion*>::iterator iter = std::find(_motions.begin(), _motions.end(), motion);
+
+	// if found remove
+	if (iter != _motions.end())
+	{
+		_motions.erase(iter);
+	}
+}
+
+int SBHandConfiguration::getNumMotions()
+{
+	return _motions.size();
+}
+
+void SBHandConfiguration::printMotionNames()
+{
+	std::vector<SBMotion*>::iterator iter = _motions.begin();
+	for (; iter != _motions.end() ; iter++)
+	{
+		SBMotion* motion = *iter;
+		LOG("Motion name is %s", motion->getName());
+	}
+}
+
+SBMotion* SBHandConfiguration::getMotion(int i)
+{
+	if (i >= _motions.size())
+	{
+		LOG("i is greater than motion vector size");
+		return NULL;
+	}
+
+	return _motions[i];
+}
+
+
+}
---
+++ core/smartbody/SmartBody/src/sb/SBHandConfiguration.h
@@ -0,0 +1,30 @@
+#ifndef _SBHANDCONFIGURATION_H_
+#define _SBHANDCONFIGURATION_H_
+
+#include <sb/SBScene.h>
+#include <string>
+
+namespace SmartBody{
+
+
+class SBHandConfiguration{
+
+public:
+	SBAPI SBHandConfiguration();
+	SBAPI ~SBHandConfiguration();
+
+	SBAPI void addMotion(std::string motionName);
+	SBAPI void removeMotion(std::string motionName);
+	SBAPI int getNumMotions();
+	SBAPI void printMotionNames();
+	SBAPI SBMotion* getMotion(int i);
+
+private:
+	
+	// loaded motions
+	std::vector<SBMotion*> _motions;
+};
+
+}
+
+#endif
