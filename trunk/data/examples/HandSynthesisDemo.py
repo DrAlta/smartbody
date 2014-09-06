50e3c31f7e62d3115e6898f9b56bf49e382c29ac
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-09-06 00:35:37 +0000

    Changes to hand synthesis demo

--- trunk/data/examples/HandSynthesisDemo.py
+++ trunk/data/examples/HandSynthesisDemo.py
@@ -1,6 +1,10 @@
 print "|--------------------------------------------|"
 print "|        Setup Hand Demo 			        |"
 print "|--------------------------------------------|"
+print ""
+print "Demonstrates using a set of gestures to synthesize hand and finger motion on top of a source motion that does not contain hand or finger animation data."
+		
+		
 
 		
 def setMotionNameSkeleton(motionName, skelName):
@@ -47,11 +51,29 @@ scene.addAssetPath('script', 'behaviorsets')
 scene.addAssetPath('script', 'scripts')
 scene.loadAssets()
 
+
+# configure camera
+# Set scene parameters and camera
+print 'Configuring scene parameters and camera'
+scene.setScale(1.0)
+scene.setBoolAttribute('internalAudio', True)
+scene.run('default-viewer.py')
+camera = getCamera()
+camera.setEye(0, 1.71, 1.86)
+camera.setCenter(0, 1, 0.01)
+camera.setUpVector(SrVec(0, 1, 0))
+camera.setScale(1)
+camera.setFov(1.0472)
+camera.setFarPlane(100)
+camera.setNearPlane(0.1)
+camera.setAspectRatio(0.966897)
+cameraPos = SrVec(0, 1.6, 10)
+scene.getPawn('camera').setPosition(cameraPos)
+
 # Runs the default viewer for camera
 scene.setScale(1)
 
-# Apply hand joint map
-# Set up joint map for Bradh  
+# Set up joint map for character  
 print 'Setting up joint map and configuring Brad\'s skeleton'
 scene.run('zebra2-map.py')
 zebra2Map = scene.getJointMapManager().getJointMap('zebra2')
@@ -59,20 +81,21 @@ bradSkeleton = scene.getSkeleton('ChrBrad.sk')
 zebra2Map.applySkeleton(bradSkeleton)
 zebra2Map.applyMotionRecurse('ChrBrad')
 
-# setting up ipy retargetting
+# setting up retargetting between the source motion and the current character
 print 'Setting up joint map and configuring Brad\'s skeleton'
 scene.run('ipisoft-map.py')
 ipisoftMap = scene.getJointMapManager().getJointMap('ipisoft')
 
 
-# retargetting the motion (iPy motion to smartbody skeleton)
+# retargetting the motion (iPi motion to SmartBody skeleton)
 m = scene.getMotion("Adil_Gesture1.bvh")
 ipisoftMap.applyMotion(m)
 ipiSk = scene.getSkeleton("Adil_Gesture1.bvh")
 ipisoftMap.applySkeleton(ipiSk)
 setMotionNameSkeleton('Adil_Gesture1.bvh', 'Adil_Gesture1.bvh')
 createRetargetInstance('Adil_Gesture1.bvh','ChrBrad.sk')
-
+# at this point, the source motion has been retargeted to the character,
+# so it can be used to generate the hand animation
 
 # run behavior set to load motions
 scene.run('BehaviorSetGestures.py')
@@ -81,20 +104,26 @@ retargetBehaviorSet('ChrBrad')
 
 # create a new hand configuration
 configManager = scene.getHandConfigurationManager()
-handConfig = configManager.createHandConfiguration("test_config")
+handConfig = configManager.createHandConfiguration("singlegesture")
 handConfig.addMotion("ChrBrad@Idle01_NegativeBt01")
-#configManager.printHandConfiguration("test_config")
-
+configMotions = handConfig.getMotionNames()
+#print "Motions in singlegesture configuration:"
+#for m in range(0, len(configMotions)):
+#	print configMotions[m]
+	
 #create another hand configuration
-handConfig2 = configManager.createHandConfiguration("new_config")
+handConfig2 = configManager.createHandConfiguration("multiplegestures")
 handConfig2.addMotion("ChrBrad@Idle01_YouPointLf01")
 handConfig2.addMotion("ChrBrad@Idle01_BeatHighBt01")
 handConfig2.addMotion("ChrBrad@Idle01_IndicateRightBt01")
-#configManager.printHandConfiguration("new_config")
-
+print "Motions in multiplegestures configuration:"
+#configMotions = handConfig2.getMotionNames()
+#for m in range(0, len(configMotions)):
+#	print configMotions[m]
+	
 # create a third detailed hand configuration
 print 'Setting up all_config'
-handConfig3 = configManager.createHandConfiguration("all_config")
+handConfig3 = configManager.createHandConfiguration("allgestures")
 handConfig3.addMotion("ChrBrad@Idle01_YouPointLf01")
 handConfig3.addMotion("ChrBrad@Idle01_BeatHighBt01")
 handConfig3.addMotion("ChrBrad@Idle01_ArmStretch01")
@@ -131,9 +160,10 @@ handConfig3.addMotion("ChrBrad@Idle01_SafeRt01")
 handConfig3.addMotion("ChrBrad@Idle01_ScratchChest01")
 handConfig3.addMotion("ChrBrad@Idle01_ScratchHeadLf01")
 handConfig3.addMotion("ChrBrad@Idle01_YouLf01")
-
-# use this to print hand configurations
-#configManager.printHandConfiguration("all_config")
+print "Motions in multiplegestures configuration:"
+#configMotions = handConfig3.getMotionNames()
+#for m in range(0, len(configMotions)):
+#	print configMotions[m]
 
 # Add ChrBrad (source character )
 source = scene.createCharacter('source', '')
@@ -146,18 +176,23 @@ source.createStandardControllers()
 # Deformable mesh
 source.setDoubleAttribute('deformableMeshScale', 0.01)
 
-# enable these for mesh
+# enable these to show the deformable mesh, otherwise only the skeleton will be shown
 #source.setStringAttribute('deformableMesh', 'ChrMaarten.dae')
 #source.setStringAttribute("displayType", "GPUmesh")
 
-# configure camera
-print 'Configuring scene parameters and camera'
-camera = getCamera()
-camera.setEye(0, 7.98, 17.44)
-camera.setCenter(1.0, 1.7, -39.5)
-camera.setUpVector(SrVec(0, 1, 0))
-camera.setScale(1)
-camera.setFov(1.0472)
-camera.setFarPlane(1000)
-camera.setNearPlane(0.1)
-camera.setAspectRatio(1.02)
+
+# set the hand configuration debug to display what is happening
+#source.setBoolAttribute("hand.debug", True)
+# decide which hand configuration to use
+source.setStringAttribute("hand.config", "allgestures")
+source.setStringAttribute("hand.config", "multiplegestures")
+source.setStringAttribute("hand.config", "singlegestures")
+# set the name of the motion to use
+source.setStringAttribute("hand.motionName", "Adil_Gesture1.bvh")
+
+
+
+
+
+
+
