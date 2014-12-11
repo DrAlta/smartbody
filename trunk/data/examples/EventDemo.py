b9f519f18de720b43ddca10f952ec85e27d0911c
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2014-12-11 07:04:39 +0000

    More fixes to detect Windows 8 voices in example scripts

--- trunk/data/examples/EventDemo.py
+++ trunk/data/examples/EventDemo.py
@@ -93,7 +93,7 @@ if platform.system() == "Windows":
 	if windowsVer.find("Windows-7") == 0:
 		rachel.setVoiceCode('Microsoft|Anna')
 	else:
-		if windowsVer.find("Windows-8") == 0 or windowsVer.find("Windows-post2008server") == 0:
+		if windowsVer.find("Windows-8") == 0 or windowsVer.find("Windows-post2008Server") == 0:
 			rachel.setVoiceCode('Microsoft|Zira|Desktop')
 else: # non-Windows platform, use Festival voices
 	rachel.setVoiceCode('voice_kal_diphone')
