0b62e8645351dc1e76d0e47b75bce4da11ef169f
fengww <fengww@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2015-03-25 19:25:04 +0000

    Fixes for Android port to add wes-gl wrapper ( for GLES 2.0 ) and Assimp loader into the build.

--- trunk/android/smartbody/jni/Application.mk
+++ trunk/android/smartbody/jni/Application.mk
@@ -1,5 +1,7 @@
 #APP_STL := stlport_static
 APP_STL := gnustl_static
 #APP_ABI := armeabi-v7a
-APP_PLATFORM := android-10
-
+APP_PLATFORM := android-12
+APP_OPTIM := release
+#APP_CPPFLAGS := -O3 -std=c++11 -fexceptions -fpermissive -frtti -DBOOST_NO_CXX11_SCOPED_ENUMS -DSB_NO_ASSIMP
+APP_CPPFLAGS := -O3 -std=c++11 -fexceptions -fpermissive -frtti -DBOOST_NO_CXX11_SCOPED_ENUMS
