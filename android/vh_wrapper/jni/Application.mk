4446714a5d5e08849f0a7cd68950fd1dd8c93392
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2015-04-17 17:21:10 +0000

    updated vhwrapper for Android

--- android/vh_wrapper/jni/Application.mk
+++ android/vh_wrapper/jni/Application.mk
@@ -1,7 +1,7 @@
 #APP_STL := stlport_static
 APP_STL := gnustl_static
 #APP_ABI := armeabi-v7a
-APP_PLATFORM := android-10
+APP_PLATFORM := android-9
 APP_MODULES := libvhwrapper
-NDK_TOOLCHAIN_VERSION := 4.4.3
+NDK_TOOLCHAIN_VERSION := 4.6
 APP_SHORT_COMMANDS := true
