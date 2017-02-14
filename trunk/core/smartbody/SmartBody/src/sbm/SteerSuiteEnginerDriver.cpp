7c7b3c66adc2872fed52f0a6c9f921d94f3fe7f1
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2017-02-14 01:54:17 +0000

    added copyright info to sb/ files.

--- trunk/core/smartbody/SmartBody/src/sbm/SteerSuiteEnginerDriver.cpp
+++ trunk/core/smartbody/SmartBody/src/sbm/SteerSuiteEnginerDriver.cpp
@@ -1,14 +1,23 @@
-//
-// Copyright (c) 2009-2010 Shawn Singh, Mubbasir Kapadia, Petros Faloutsos, Glenn Reinman
-// See license.txt for complete license.
-//
+/*************************************************************
+Copyright (C) 2017 University of Southern California
 
-/// @file SteerSuiteEngineDriver.cpp
-/// @brief Implements the SteerSuiteEngineDriver functionality.
-///
-/// @todo
-///   - update documentation in this file
-///
+This file is part of Smartbody.
+
+Smartbody is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Smartbody is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Smartbody.  If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************/
+
 #include "SteerSuiteEngineDriver.h"
 
 #include <iostream>
--- trunk/core/smartbody/SmartBody/src/sbm/sbm_constants.h
+++ trunk/core/smartbody/SmartBody/src/sbm/sbm_constants.h
@@ -1,27 +1,22 @@
-/*
- *  sbm_constants.h - part of SmartBody-lib
- *  Copyright (C) 2008  University of Southern California
- *
- *  SmartBody-lib is free software: you can redistribute it and/or
- *  modify it under the terms of the Lesser GNU General Public License
- *  as published by the Free Software Foundation, version 3 of the
- *  license.
- *
- *  SmartBody-lib is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  Lesser GNU General Public License for more details.
- *
- *  You should have received a copy of the Lesser GNU General Public
- *  License along with SmartBody-lib.  If not, see:
- *      http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- *  CONTRIBUTORS:
- *      Marcelo Kallmann, USC (currently at UC Merced)
- *      Marcus Thiebaux, USC
- *      Andrew n marshall, USC
- *      Ashok Basawapatna, USC (no longer)
- */
+/*************************************************************
+Copyright (C) 2017 University of Southern California
+
+This file is part of Smartbody.
+
+Smartbody is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Smartbody is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Smartbody.  If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************/
 
 #ifndef SR_FLAG_H
 #define SR_FLAG_H
