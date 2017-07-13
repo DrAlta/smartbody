076ecac6f8aa36a6412d8683e5450d2ec6933d8b
arishapiro <arishapiro@a5645e14-eb4a-0410-beb5-ab350a9d4ef9>
2017-07-13 20:54:00 +0000

    removed sbjniapp

--- android/sbjniapp/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-#APP_STL := stlport_static
-APP_STL := gnustl_static
-APP_ABI := armeabi-v7a
-NDK_TOOLCHAIN_VERSION := 4.8
-APP_PLATFORM := android-18
-APP_MODULES := sbjniapp
-APP_OPTIM := release
-APP_CPPFLAGS := -O3 -std=c++11 -fexceptions -fpermissive -frtti -DBOOST_NO_CXX11_SCOPED_ENUMS
